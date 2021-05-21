8404823f6ff5fc9d4d5d8711c715508fb2e10722
lns13301 <rkdrbgks22@ks.ac.kr>
2021-05-21 15:57:48 +0900

    chore: propertySource 지정

--- src/main/java/wooteco/subway/SubwayApplication.java
+++ src/main/java/wooteco/subway/SubwayApplication.java
@@ -2,8 +2,10 @@ package wooteco.subway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-prod.properties")
 public class SubwayApplication {
 
 	public static void main(String[] args) {
