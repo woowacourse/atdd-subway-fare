08e0f0b176a9cd2e55292042594b346351359aba
YeongBeom Heo <hybeom@gmail.com>
2021-05-21 15:05:38 +0900

    test(FareTest) : 요금 생성 관련 테스트 작성

---
+++ src/main/java/wooteco/subway/path/domain/Fare.java
@@ -0,0 +1,16 @@
+package wooteco.subway.path.domain;
+
+public class Fare {
+
+    public Fare() {
+
+    }
+
+    public Fare(int distance) {
+
+    }
+
+    public int value() {
+        return 0;
+    }
+}
