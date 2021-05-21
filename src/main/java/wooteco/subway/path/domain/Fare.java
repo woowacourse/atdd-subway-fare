0700a35cfa9baf8ba809ace44c2fca1699fe34d5
seunghyun <ggyool@naver.com>
2021-05-21 15:55:34 +0900

    feat(Fare): 거리에 따른 요금 계산 기능 구현

--- src/main/java/wooteco/subway/path/domain/Fare.java
+++ src/main/java/wooteco/subway/path/domain/Fare.java
@@ -2,15 +2,37 @@ package wooteco.subway.path.domain;
 
 public class Fare {
 
-    public Fare() {
+    private static final int BASIC_DISTANCE = 10;
+    private static final int EXCEED_INSTANCE = 40;
+    private static final int BASIC_FARE = 1250;
+    private static final int EXTRA_FIRST_DISTANCE_UNIT = 5;
+    private static final int EXTRA_SECOND_DISTANCE_UNIT = 8;
+    private static final int EXTRA_FARE_UNIT = 100;
+    private static final int EXTRA_FIRST_FULL_FARE = 800;
 
-    }
+    private final int value;
 
     public Fare(int distance) {
+        this.value = calculateFare(distance);
+    }
 
+    private int calculateFare(int distance) {
+        if (distance < BASIC_DISTANCE) {
+            return BASIC_FARE;
+        }
+        return BASIC_FARE + calculateOverFare(distance);
+    }
+
+    private int calculateOverFare(int distance) {
+        if (distance - BASIC_DISTANCE - EXCEED_INSTANCE < 0) {
+            return (int) (
+                (Math.ceil((distance - BASIC_DISTANCE - 1) / EXTRA_FIRST_DISTANCE_UNIT) + 1)
+                    * EXTRA_FARE_UNIT);
+        }
+        return (int) ((Math.ceil((distance - BASIC_DISTANCE - EXCEED_INSTANCE - 1) / EXTRA_SECOND_DISTANCE_UNIT) + 1) * EXTRA_FARE_UNIT) + EXTRA_FIRST_FULL_FARE;
     }
 
     public int value() {
-        return 0;
+        return this.value;
     }
 }
