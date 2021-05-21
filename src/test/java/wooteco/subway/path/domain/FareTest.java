958447c8a93c43c10ffddb31c1935a4c8b4869bf
YeongBeom Heo <hybeom@gmail.com>
2021-05-21 15:05:38 +0900

    test(FareTest) : 요금 생성 관련 테스트 작성

---
+++ src/test/java/wooteco/subway/path/domain/FareTest.java
@@ -0,0 +1,36 @@
+package wooteco.subway.path.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class FareTest {
+
+    @ParameterizedTest
+    @DisplayName("기본 요금 계산")
+    @MethodSource("matchedFare")
+    void defaultRate(int distance, int predictedFare) {
+        //given
+
+        //when
+        Fare fare = new Fare(distance);
+
+        //then
+        assertThat(fare.value()).isEqualTo(predictedFare);
+    }
+
+    private static Stream<Arguments> matchedFare() {
+        return Stream.of(
+            Arguments.of(9, 1250),
+            Arguments.of(12, 1350),
+            Arguments.of(66, 2250)
+        );
+    }
+
+}
