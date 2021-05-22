56c3ea2b533bf8ee4661ae7b72af9c819db0db82
Sol ji Choi <bssj9307@gmail.com>
2021-05-22 19:53:28 +0900

    refactor: DataLoader 수정
    - 테이블이 비어있을 때만 데이터 삽입하도록 변경
    - schema.sql 에서 drop 쿼리 제거

--- src/main/java/wooteco/subway/DataLoader.java
+++ src/main/java/wooteco/subway/DataLoader.java
@@ -1,5 +1,6 @@
 package wooteco.subway;
 
+import java.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -29,6 +30,10 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        List<Line> lines = lineDao.findAll();
+        if (lines.size() > 0) {
+            return;
+        }
         Station 강남역 = stationDao.insert(new Station("강남역"));
         Station 판교역 = stationDao.insert(new Station("판교역"));
         Station 정자역 = stationDao.insert(new Station("정자역"));
