19ef3e3d7f184aff0a18392485ef1aa170f2f08f
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-22 21:34:22 +0900

    test(Path): 연령대별 요금 테스트 추가

--- src/test/java/wooteco/subway/path/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/path/PathAcceptanceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
+import wooteco.subway.auth.dto.TokenResponse;
 import wooteco.subway.line.dto.LineResponse;
 import wooteco.subway.path.dto.PathResponse;
 import wooteco.subway.station.dto.StationResponse;
@@ -18,12 +19,23 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.subway.auth.AuthAcceptanceTest.로그인되어_있음;
 import static wooteco.subway.line.LineAcceptanceTest.지하철_노선_등록되어_있음;
 import static wooteco.subway.line.SectionAcceptanceTest.지하철_구간_등록되어_있음;
+import static wooteco.subway.member.MemberAcceptanceTest.회원_생성됨;
+import static wooteco.subway.member.MemberAcceptanceTest.회원_생성을_요청;
 import static wooteco.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
+    public static final String EMAIL1 = "email1@email.com";
+    public static final String EMAIL2 = "email2@email.com";
+    public static final String EMAIL3 = "email3@email.com";
+    public static final String PASSWORD = "password";
+    public static final int AGE_UNDER_6 = 5;
+    public static final int AGE_IN_BETWEEN_6_TO_13 = 9;
+    public static final int AGE_IN_BETWEEN_13_TO_19 = 15;
+
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
@@ -40,6 +52,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private StationResponse 구의역;
     private StationResponse 성수역;
     private StationResponse 뚝섬역;
+    TokenResponse 초딩;
+    TokenResponse 중고딩;
+    TokenResponse 애기;
 
     /*
      * 교대역    --- *2호선*10km---   강남역
@@ -72,6 +87,16 @@ public class PathAcceptanceTest extends AcceptanceTest {
     public void setUp() {
         super.setUp();
 
+        ExtractableResponse<Response> createResponse1 = 회원_생성을_요청(EMAIL1, PASSWORD, AGE_UNDER_6);
+        회원_생성됨(createResponse1);
+        애기 = 로그인되어_있음(EMAIL1, PASSWORD);
+        ExtractableResponse<Response> createResponse2 = 회원_생성을_요청(EMAIL2, PASSWORD, AGE_IN_BETWEEN_6_TO_13);
+        회원_생성됨(createResponse2);
+        초딩 = 로그인되어_있음(EMAIL2, PASSWORD);
+        ExtractableResponse<Response> createResponse3 = 회원_생성을_요청(EMAIL3, PASSWORD, AGE_IN_BETWEEN_13_TO_19);
+        회원_생성됨(createResponse3);
+        중고딩 = 로그인되어_있음(EMAIL3, PASSWORD);
+
         강남역 = 지하철역_등록되어_있음("강남역");
         양재역 = 지하철역_등록되어_있음("양재역");
         교대역 = 지하철역_등록되어_있음("교대역");
@@ -98,11 +123,47 @@ public class PathAcceptanceTest extends AcceptanceTest {
         지하철_구간_등록되어_있음(육호선, 뚝섬역, 구의역, 50);
     }
 
+    @DisplayName("애기 - 두 역의 최단 거리 경로를 조회한다.(추가운임(55km))")
+    @Test
+    void findPathByDistance55kmUnder6() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(애기, 3L, 5L);
+
+        //then
+        적절한_경로_응답됨(response, Lists.newArrayList(교대역, 남부터미널역, 양재역, 멀리있는역));
+        총_거리가_응답됨(response, 55);
+        총_요금이_응답됨(response, 0);
+    }
+
+    @DisplayName("초딩 - 두 역의 최단 거리 경로를 조회한다.(추가운임(55km))")
+    @Test
+    void findPathByDistance55kmInBetween6To13() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(초딩, 3L, 5L);
+
+        //then
+        적절한_경로_응답됨(response, Lists.newArrayList(교대역, 남부터미널역, 양재역, 멀리있는역));
+        총_거리가_응답됨(response, 55);
+        총_요금이_응답됨(response, 900);
+    }
+
+    @DisplayName("중고딩 - 두 역의 최단 거리 경로를 조회한다.(추가운임(55km))")
+    @Test
+    void findPathByDistance55kmInBetween13To19() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(중고딩, 3L, 5L);
+
+        //then
+        적절한_경로_응답됨(response, Lists.newArrayList(교대역, 남부터미널역, 양재역, 멀리있는역));
+        총_거리가_응답됨(response, 55);
+        총_요금이_응답됨(response, 1440);
+    }
+
     @DisplayName("두 역의 최단 거리 경로를 조회한다.(추가운임(55km))")
     @Test
     void findPathByDistance55km() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(3L, 5L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""), 3L, 5L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(교대역, 남부터미널역, 양재역, 멀리있는역));
@@ -114,7 +175,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance12km() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(1L, 4L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""), 1L, 4L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(강남역, 양재역, 남부터미널역));
@@ -126,7 +187,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance5km() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(3L, 2L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""), 3L, 2L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(교대역, 남부터미널역, 양재역));
@@ -138,7 +199,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance5kmWithExtraFare() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(6L, 7L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""), 6L, 7L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(잠실역, 강변역));
@@ -150,7 +211,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance61kmWithExtraFare() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(6L, 10L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""), 6L, 10L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(잠실역, 강변역, 구의역, 뚝섬역));
@@ -162,7 +223,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
     @Test
     void findPathByDistance11kmWithExtraFare() {
         //when
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(6L, 8L);
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(new TokenResponse(""),6L, 8L);
 
         //then
         적절한_경로_응답됨(response, Lists.newArrayList(잠실역, 강변역, 구의역));
@@ -170,9 +231,10 @@ public class PathAcceptanceTest extends AcceptanceTest {
         총_요금이_응답됨(response, 2250);
     }
 
-    public static ExtractableResponse<Response> 거리_경로_조회_요청(long source, long target) {
+    public static ExtractableResponse<Response> 거리_경로_조회_요청(TokenResponse tokenResponse, long source, long target) {
         return RestAssured
                 .given().log().all()
+                .auth().oauth2(tokenResponse.getAccessToken())
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().get("/paths?source={sourceId}&target={targetId}", source, target)
                 .then().log().all()
