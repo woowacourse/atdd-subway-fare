b76ec56e0094a770921c2a78ef2fa1af3939853a
Sol ji Choi <bssj9307@gmail.com>
2021-05-22 19:19:19 +0900

    feat: 인증 테스트 작성과 문서화

---
+++ src/test/java/wooteco/subway/auth/ui/AuthControllerTest.java
@@ -0,0 +1,57 @@
+package wooteco.subway.auth.ui;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import wooteco.subway.auth.application.AuthService;
+import wooteco.subway.auth.dto.TokenRequest;
+import wooteco.subway.auth.dto.TokenResponse;
+
+@WebMvcTest(controllers = AuthController.class)
+@ActiveProfiles("test")
+@AutoConfigureRestDocs
+public class AuthControllerTest {
+
+    @MockBean
+    private AuthService authService;
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("로그인 - 성공")
+    @Test
+    public void login() throws Exception {
+        //given
+        TokenRequest tokenRequest = new TokenRequest("test@test.com", "password");
+        TokenResponse tokenResponse = new TokenResponse("아주강력한비밀번호");
+        given(authService.login(any())).willReturn(tokenResponse);
+
+        //when
+        mockMvc.perform(post("/api/login/token")
+            .content(objectMapper.writeValueAsString(tokenRequest))
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+        )
+            // then
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("accessToken").value(tokenResponse.getAccessToken()))
+            .andDo(print())
+            .andDo(document("auth-login"));
+    }
+}
