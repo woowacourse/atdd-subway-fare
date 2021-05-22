cc810b3c27024610cc99337cba3528b0c1eb22f4
yjksw <cairofaith@gmail.com>
2021-05-22 19:53:07 +0900

    refactor: 테이블에 데이터 존재 시, DataLoader 미실행

--- src/main/java/wooteco/subway/DataLoader.java
+++ src/main/java/wooteco/subway/DataLoader.java
@@ -29,6 +29,12 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        final int size = lineDao.findAll().size();
+
+        if (size > 0) {
+            return;
+        }
+
         Station 강남역 = stationDao.insert(new Station("강남역"));
         Station 판교역 = stationDao.insert(new Station("판교역"));
         Station 정자역 = stationDao.insert(new Station("정자역"));
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,3 @@
-drop table if exists LINE;
-drop table if exists SECTION;
-drop table if exists STATION;
-drop table if exists MEMBER;
-
 create table if not exists STATION
 (
     id bigint auto_increment not null,
