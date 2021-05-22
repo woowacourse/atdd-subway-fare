b19b43b41256bf03f4a32b61bc3b75c62db50fdf
xrabcde <xrabcde@gmail.com>
2021-05-22 20:50:29 +0900

    refactor(LineTest): 테스트에서 Fixture를 사용하도록 수정

--- src/test/java/wooteco/subway/line/LineTest.java
+++ src/test/java/wooteco/subway/line/LineTest.java
@@ -3,27 +3,22 @@ package wooteco.subway.line;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import wooteco.subway.line.domain.Line;
-import wooteco.subway.station.domain.Station;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.subway.line.LineFixture.이호선;
+import static wooteco.subway.station.StationFixture.*;
 
 public class LineTest {
-    Station 양재역 = new Station(2L, "양재역");;
-    Station 교대역 = new Station(3L, "교대역");;
-    Station 남부터미널역 = new Station(4L, "남부터미널역");;
-
-    Line 삼호선 = new Line("삼호선", "brown");
 
     @BeforeEach
     void setUp() {
-        삼호선.addSection(교대역, 남부터미널역, 3);
-        삼호선.addSection(남부터미널역, 양재역, 2);
+        이호선.addSection(강남역, 잠실역, 3);
+        이호선.addSection(잠실역, 구의역, 2);
     }
 
     @Test
     @DisplayName("노선의 길이를 잘 반환하는지 확인한다.")
     void totalDistance() {
-        assertThat(삼호선.distance()).isEqualTo(5);
+        assertThat(이호선.distance()).isEqualTo(5);
     }
 }
--- src/test/java/wooteco/subway/line/SectionFixture.java
+++ src/test/java/wooteco/subway/line/SectionFixture.java
@@ -6,6 +6,6 @@ import static wooteco.subway.station.StationFixture.*;
 
 public class SectionFixture {
     public static Section 왕십리_잠실_거리22 = new Section(왕십리역, 잠실역, 22);
-    public static Section 잠실_강남_거리25 = new Section( 잠실역, 강남역, 25);
+    public static Section 잠실_강남_거리25 = new Section(잠실역, 강남역, 25);
     public static Section 강남_구의_거리10 = new Section(강남역, 구의역, 10);
 }
