6031fa98bcdde6f7513796f9b57c37486bcdc94f
Gomding <backchida@gmail.com>
2021-05-22 16:57:01 +0900

    refactor(SubwayControllerAdvice.java) : Exception 핸들링하여 예상치 못한 예외를 잡도록 구현

--- src/main/java/wooteco/subway/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/SubwayControllerAdvice.java
@@ -1,5 +1,6 @@
 package wooteco.subway;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,11 +9,17 @@ import wooteco.subway.exception.dto.ExceptionResponse;
 
 @RestControllerAdvice
 public class SubwayControllerAdvice {
-
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<ExceptionResponse> subwayException(SubwayException subwayException) {
         return ResponseEntity
                 .status(subwayException.getHttpStatus())
                 .body(new ExceptionResponse(subwayException.getMessage(), subwayException.getHttpStatus().value()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> exception(Exception exception) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ExceptionResponse(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
     }
 }
