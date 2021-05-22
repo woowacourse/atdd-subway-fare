141792a909c1bc3de4cb70dc21682f08e912088d
Daeun Lee <leede0418@likelion.org>
2021-05-22 17:44:12 +0900

    refactor(exception): @ControllerAdvice 수정

--- src/main/java/wooteco/subway/exception/SubwayAdvice.java
+++ src/main/java/wooteco/subway/exception/SubwayAdvice.java
@@ -1,15 +1,24 @@
 package wooteco.subway.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import wooteco.subway.exception.auth.InvalidTokenException;
 
 @RestControllerAdvice
 public class SubwayAdvice {
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<SubwayExceptionResponse> subwayException(SubwayException e) {
         SubwayExceptionResponse subwayExceptionResponse =
-                new SubwayExceptionResponse(e.getHttpStatus(), e.getMessage());
-        return ResponseEntity.ok(subwayExceptionResponse);
+                new SubwayExceptionResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(subwayExceptionResponse);
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<SubwayExceptionResponse> invalidTokenException(InvalidTokenException e) {
+        SubwayExceptionResponse subwayExceptionResponse =
+                new SubwayExceptionResponse(HttpStatus.UNAUTHORIZED.value(), e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED.value()).body(subwayExceptionResponse);
     }
 }
--- src/main/java/wooteco/subway/exception/SubwayException.java
+++ src/main/java/wooteco/subway/exception/SubwayException.java
@@ -1,18 +1,10 @@
 package wooteco.subway.exception;
 
-import org.springframework.http.HttpStatus;
-
 public class SubwayException extends RuntimeException {
-    private final int httpStatus;
     private final String message;
 
     public SubwayException(String message) {
-        this.httpStatus = HttpStatus.BAD_REQUEST.value();
         this.message = message;
-    }
-
-    public int getHttpStatus() {
-        return httpStatus;
     }
 
     public String getMessage() {
