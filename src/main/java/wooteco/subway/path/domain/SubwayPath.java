a2058c59ed17e7e17a27f65125e4ff50c139385a
kimjunseo <air.junseo@gmail.com>
2021-05-22 17:07:40 +0900

    feat : 요금 계산 기능 구현 및 테스트 코드 작성

--- src/main/java/wooteco/subway/path/domain/SubwayPath.java
+++ src/main/java/wooteco/subway/path/domain/SubwayPath.java
@@ -24,4 +24,21 @@ public class SubwayPath {
     public int calculateDistance() {
         return sectionEdges.stream().mapToInt(it -> it.getSection().getDistance()).sum();
     }
+
+    public int calculateFare() {
+        int distance = calculateDistance();
+        int fare = 1250;
+        if (distance <= 10) {
+            return fare;
+        }
+        if (distance <= 50) {
+            return fare + calculateOverFare(distance - 10, 5);
+        }
+        fare += calculateOverFare(40, 5);
+        return fare + calculateOverFare(distance - 50, 8);
+    }
+
+    private int calculateOverFare(int distance, int unit) {
+        return (int) ((Math.ceil((distance - 1) / unit) + 1) * 100);
+    }
 }
---
+++ src/test/java/wooteco/subway/path/domain/SubwayPathTest.java
@@ -0,0 +1,87 @@
+package wooteco.subway.path.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static wooteco.subway.line.LineFixture.이호선;
+import static wooteco.subway.line.SectionFixture.*;
+
+class SubwayPathTest {
+
+    @DisplayName("요금 계산 - 50km 초과시")
+    @Test
+    void calculateFare() {
+        // given
+        List<SectionEdge> sectionEdges = Arrays.asList(
+                new SectionEdge(
+                        왕십리_잠실_거리22,
+                        이호선
+                ),
+                new SectionEdge(
+                        잠실_강남_거리25,
+                        이호선
+                ),
+                new SectionEdge(
+                        강남_구의_거리10,
+                        이호선
+                )
+        );
+
+        SubwayPath subwayPath = new SubwayPath(sectionEdges, Collections.emptyList());
+
+        // when
+        int fare = subwayPath.calculateFare();
+
+        // then
+        assertThat(fare).isEqualTo(2150);
+    }
+
+    @DisplayName("요금 계산 - 10km ~ 50km")
+    @Test
+    void calculateFare2() {
+        // given
+        List<SectionEdge> sectionEdges = Arrays.asList(
+                new SectionEdge(
+                        왕십리_잠실_거리22,
+                        이호선
+                ),
+                new SectionEdge(
+                        잠실_강남_거리25,
+                        이호선
+                )
+        );
+
+        SubwayPath subwayPath = new SubwayPath(sectionEdges, Collections.emptyList());
+
+        // when
+        int fare = subwayPath.calculateFare();
+
+        // then
+        assertThat(fare).isEqualTo(2050);
+    }
+
+    @DisplayName("요금 계산 - 10km 이내")
+    @Test
+    void calculateFare3() {
+        // given
+        List<SectionEdge> sectionEdges = Collections.singletonList(
+                new SectionEdge(
+                        강남_구의_거리10,
+                        이호선
+                )
+        );
+
+        SubwayPath subwayPath = new SubwayPath(sectionEdges, Collections.emptyList());
+
+        // when
+        int fare = subwayPath.calculateFare();
+
+        // then
+        assertThat(fare).isEqualTo(1250);
+    }
+}
