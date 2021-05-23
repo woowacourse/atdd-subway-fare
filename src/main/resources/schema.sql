3a87e6de5a65bd7f75d99ce83285a1f3b3efc1ea
TaeJeong Kim <xowjd41@naver.com>
2021-05-23 21:40:48 +0900

    refactor: sql 문 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
-drop table if exists STATION:
-drop table if exists LINE:
-drop table if exists SECTION:
-drop table if exists MEMBER:
+drop table if exists STATION;
+drop table if exists LINE;
+drop table if exists SECTION;
+drop table if exists MEMBER;
 
 create table if not exists STATION
 (
