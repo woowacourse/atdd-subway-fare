610604eb053c3d80926364670f228baa125eb4a1
dwl5 <ssop6403@gmail.com>
2021-05-23 20:44:58 +0900

    feat: AuthenticationPrincipalConfig 추가

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.ui.AuthenticationPrincipalArgumentResolver;
+import wooteco.subway.auth.ui.AuthorizationMemberPrincipalArgumentResolver;
 
 import java.util.List;
 
@@ -19,10 +20,16 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
         argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
+        argumentResolvers.add(createAuthorizationMemberPrincipalArgumentResolver());
     }
 
     @Bean
     public AuthenticationPrincipalArgumentResolver createAuthenticationPrincipalArgumentResolver() {
         return new AuthenticationPrincipalArgumentResolver(authService);
+    }
+
+    @Bean
+    public AuthorizationMemberPrincipalArgumentResolver createAuthorizationMemberPrincipalArgumentResolver() {
+        return new AuthorizationMemberPrincipalArgumentResolver(authService);
     }
 }
--- src/main/java/wooteco/subway/path/ui/PathController.java
+++ src/main/java/wooteco/subway/path/ui/PathController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.auth.domain.AuthorizationMemberPrincipal;
+import wooteco.subway.member.domain.LoginMember;
 import wooteco.subway.path.application.PathService;
 import wooteco.subway.path.dto.PathResponse;
 
@@ -18,7 +20,9 @@ public class PathController {
     }
 
     @GetMapping("/paths")
-    public ResponseEntity<PathResponse> findPath(@RequestParam Long source, @RequestParam Long target) {
+    public ResponseEntity<PathResponse> findPath(@AuthorizationMemberPrincipal LoginMember loginMember,
+                                                 @RequestParam Long source, @RequestParam Long target) {
+
         return ResponseEntity.ok(pathService.findPath(source, target));
     }
 }
