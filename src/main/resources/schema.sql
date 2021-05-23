7ccb637a739fe41de467aaf032478bbf4dc7bd06
ecsimsw <ecsimsw@gmail.com>
2021-05-23 15:37:49 +0900

    fix : schema drop table 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+drop table if exists MEMBER;
+drop table if exists SECTION;
+drop table if exists LINE;
+drop table if exists STATION;
+
 create table if not exists STATION
 (
     id bigint auto_increment not null,
