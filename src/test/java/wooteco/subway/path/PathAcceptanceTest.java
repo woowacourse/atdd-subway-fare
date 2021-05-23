6b5fd6f94d5b0cf3b170e4b48545623e9779007c
TaeJeong Kim <xowjd41@naver.com>
2021-05-23 20:46:22 +0900

    feat: Path 예외 처리 로직

--- src/test/java/wooteco/subway/path/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/path/PathAcceptanceTest.java
@@ -7,6 +7,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
 import wooteco.subway.auth.dto.TokenResponse;
@@ -29,10 +30,14 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private LineResponse 신분당선;
     private LineResponse 이호선;
     private LineResponse 삼호선;
+    private LineResponse 우아한테크코스선;
     private StationResponse 강남역;
     private StationResponse 양재역;
     private StationResponse 교대역;
     private StationResponse 남부터미널역;
+    private StationResponse 에어포츈바다우기검프사랑해역;
+    private StationResponse 우린모두취업할거야역;
+    private StationResponse 리뷰잘부탁해요역;
     private TokenResponse tokenResponse;
 
     /**
@@ -52,10 +57,15 @@ public class PathAcceptanceTest extends AcceptanceTest {
         양재역 = 지하철역_등록되어_있음("양재역", tokenResponse);
         교대역 = 지하철역_등록되어_있음("교대역", tokenResponse);
         남부터미널역 = 지하철역_등록되어_있음("남부터미널역", tokenResponse);
+        에어포츈바다우기검프사랑해역 = 지하철역_등록되어_있음("에어포츈바다우기검프사랑해역", tokenResponse);
+        우린모두취업할거야역 = 지하철역_등록되어_있음("우린모두취업할거야역", tokenResponse);
+        리뷰잘부탁해요역 = 지하철역_등록되어_있음("리뷰잘부탁해요역", tokenResponse);
 
         신분당선 = 지하철_노선_등록되어_있음("신분당선", "bg-red-400", 강남역, 양재역, 10, tokenResponse);
         이호선 = 지하철_노선_등록되어_있음("이호선", "bg-red-500", 교대역, 강남역, 10, tokenResponse);
         삼호선 = 지하철_노선_등록되어_있음("삼호선", "bg-red-600", 교대역, 양재역, 5, tokenResponse);
+        우아한테크코스선 = 지하철_노선_등록되어_있음("우아한테크코스선", "bg-red-700", 우린모두취업할거야역, 리뷰잘부탁해요역, 5, tokenResponse);
+
 
         지하철_구간_등록되어_있음(삼호선, 교대역, 남부터미널역, 3, tokenResponse);
     }
@@ -72,11 +82,41 @@ public class PathAcceptanceTest extends AcceptanceTest {
         총_요금이_응답됨(response, 1250);
     }
 
-    public static ExtractableResponse<Response> 거리_경로_조회_요청(long source, long target) {
+    @DisplayName("departure와 arrival가 같은경우 예외가 발생한다.")
+    @Test
+    void samePositionException() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(교대역.getId(), 교대역.getId());
+
+        //then
+        거리_경로_조회_요청_실패(response);
+    }
+
+    @DisplayName("departure와 arrival가 이어지지 않는 경우 예외발생한다.")
+    @Test
+    void noPathException() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(강남역.getId(), 우린모두취업할거야역.getId());
+
+        //then
+        거리_경로_조회_요청_실패(response);
+    }
+
+    @DisplayName("departure와 arrival중 하나라도 노선에 등록되어 있지 않을 시 예외발생한다.")
+    @Test
+    void noInLineStationException() {
+        //when
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(강남역.getId(), 에어포츈바다우기검프사랑해역.getId());
+
+        //then
+        거리_경로_조회_요청_실패(response);
+    }
+
+    public static ExtractableResponse<Response> 거리_경로_조회_요청(long departure, long arrival) {
         return RestAssured
                 .given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/paths?source={sourceId}&target={targetId}", source, target)
+                .when().get("/paths?source={sourceId}&target={targetId}", departure, arrival)
                 .then().log().all()
                 .extract();
     }
@@ -103,5 +143,9 @@ public class PathAcceptanceTest extends AcceptanceTest {
     private static void 총_요금이_응답됨(ExtractableResponse<Response> response, int totalFare) {
         PathResponse pathResponse = response.as(PathResponse.class);
         assertThat(pathResponse.getFare()).isEqualTo(totalFare);
+    }
+
+    private void 거리_경로_조회_요청_실패(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
