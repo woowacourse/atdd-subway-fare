697cf779465276f9fa6eeb62387c3e34841133bc
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-24 19:24:49 +0900

    feat: displayname 추가

--- src/test/java/wooteco/subway/layer/controller/AuthControllerTest.java
+++ src/test/java/wooteco/subway/layer/controller/AuthControllerTest.java
@@ -1,10 +1,12 @@
 package wooteco.subway.layer.controller;
 
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import wooteco.subway.acceptance.auth.application.AuthService;
 import wooteco.subway.acceptance.auth.ui.AuthController;
 
+@DisplayName("인증 관련 테스트")
 @WebMvcTest(controllers = AuthController.class)
 public class AuthControllerTest extends ControllerTest {
     @MockBean
