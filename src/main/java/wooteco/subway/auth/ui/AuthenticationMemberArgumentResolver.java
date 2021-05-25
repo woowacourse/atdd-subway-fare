e8e52d98c71c8a2bd1886f7da9623bc69edde2e6
YeongBeom Heo <hybeom@gmail.com>
2021-05-25 19:14:37 +0900

    feat(AuthenticationMemberArgumentResolver) : 요금 관련 로그인 처리 추가

--- src/main/java/wooteco/subway/auth/ui/AuthenticationMemberArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationMemberArgumentResolver.java
@@ -2,25 +2,24 @@ package wooteco.subway.auth.ui;
 
 import org.springframework.core.MethodParameter;
 import wooteco.subway.auth.application.AuthService;
-import wooteco.subway.auth.application.AuthorizationException;
-import wooteco.subway.auth.domain.AuthenticationPrincipal;
+import wooteco.subway.auth.domain.AuthenticationMember;
 import wooteco.subway.member.domain.LoginMember;
 
-public class AuthenticationPrincipalArgumentResolver extends AuthenticationArgumentResolver {
+public class AuthenticationMemberArgumentResolver extends AuthenticationArgumentResolver {
 
-    public AuthenticationPrincipalArgumentResolver(AuthService authService) {
+    public AuthenticationMemberArgumentResolver(AuthService authService) {
         super(authService);
     }
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+        return parameter.hasParameterAnnotation(AuthenticationMember.class);
     }
 
     @Override
     public LoginMember validMember(LoginMember member) {
         if (member.getId() == null) {
-            throw new AuthorizationException();
+            return new LoginMember(0L, "Guest", 20);
         }
         return member;
     }
