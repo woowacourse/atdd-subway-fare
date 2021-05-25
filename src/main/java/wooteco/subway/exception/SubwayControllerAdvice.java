d366b626233518a970c37f3abccfb25578405770
Seed <hsik0225@gmail.com>
2021-05-25 16:57:53 +0900

    feat : SQL unique 예외처리

--- src/main/java/wooteco/subway/exception/SubwayControllerAdvice.java
+++ src/main/java/wooteco/subway/exception/SubwayControllerAdvice.java
@@ -1,5 +1,6 @@
 package wooteco.subway.exception;
 
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,12 +12,17 @@ import wooteco.subway.exception.dto.ErrorResponse;
 public class SubwayControllerAdvice {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException() {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("처리하지 않은 서버에러입니다 ㅠㅜ.."));
     }
 
     @ExceptionHandler(SectionException.class)
     public ResponseEntity<ErrorResponse> handleSectionException(SectionException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKeyException(DuplicateKeyException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse(e.getCause().getMessage()));
     }
 }
