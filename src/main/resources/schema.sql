433967f6eec8ebec07b19ae07e951b9a70160e31
JinYoung Park <basejin0810@naver.com>
2021-05-25 17:19:31 +0900

    feat: 배포용 설정 파일 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+drop table if exists SECTION;
+drop table if exists LINE;
+drop table if exists MEMBER;
+drop table if exists STATION;
+
 create table if not exists STATION
 (
     id bigint auto_increment not null,
@@ -10,7 +15,6 @@ create table if not exists LINE
     id bigint auto_increment not null,
     name varchar(255) not null unique,
     color varchar(20) not null,
-    extra_fare int,
     primary key(id)
 );
 
@@ -23,8 +27,7 @@ create table if not exists SECTION
     distance int not null,
     primary key(id),
     foreign key (up_station_id) references STATION(id),
-    foreign key (down_station_id) references STATION(id),
-    foreign key (line_id) references LINE(id) on delete cascade
+    foreign key (down_station_id) references STATION(id)
 );
 
 create table if not exists MEMBER
