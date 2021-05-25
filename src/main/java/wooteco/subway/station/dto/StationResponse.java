cb86d59021a8bf53a77eb4cd89fadbda8553b725
ecsimsw <ecsimsw@gmail.com>
2021-05-25 16:29:21 +0900

    feat : SimpleLineResponse 정의

--- src/main/java/wooteco/subway/station/dto/StationResponse.java
+++ src/main/java/wooteco/subway/station/dto/StationResponse.java
@@ -1,5 +1,6 @@
 package wooteco.subway.station.dto;
 
+import wooteco.subway.line.dto.SimpleLineResponse;
 import wooteco.subway.station.domain.Station;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 public class StationResponse {
     private Long id;
     private String name;
+    private List<SimpleLineResponse> includedLines;
 
     public static StationResponse of(Station station) {
         return new StationResponse(station.getId(), station.getName());
@@ -19,6 +21,12 @@ public class StationResponse {
     public StationResponse(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public StationResponse(Long id, String name, List<SimpleLineResponse> includedLines) {
+        this.id = id;
+        this.name = name;
+        this.includedLines = includedLines;
     }
 
     public static List<StationResponse> listOf(List<Station> stations) {
@@ -33,5 +41,9 @@ public class StationResponse {
 
     public String getName() {
         return name;
+    }
+
+    public List<SimpleLineResponse> getIncludedLines() {
+        return includedLines;
     }
 }
