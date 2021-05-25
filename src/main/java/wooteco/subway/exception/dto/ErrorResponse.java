eefea62e47cb9b9fa951a80f3e29f50ee617cbe4
pomo <bosl95@naver.com>
2021-05-25 18:07:47 +0900

    fix: datatype converter 오류 수정

--- src/main/java/wooteco/subway/exception/dto/ErrorResponse.java
+++ src/main/java/wooteco/subway/exception/dto/ErrorResponse.java
@@ -3,6 +3,9 @@ package wooteco.subway.exception.dto;
 public class ErrorResponse {
     private String message;
 
+    public ErrorResponse() {
+    }
+
     public ErrorResponse(String message) {
         this.message = message;
     }
