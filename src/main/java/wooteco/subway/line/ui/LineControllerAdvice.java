187811ea0bcc71069394ae4c59469f4f3bf80907
Kimun Kim <kkm97351@gmail.com>
2021-05-27 02:24:54 +0900

    feat: 노선 이름 및 색상 중복에 대한 예외 메세지를 담아 응답

--- src/main/java/wooteco/subway/line/ui/LineControllerAdvice.java
+++ src/main/java/wooteco/subway/line/ui/LineControllerAdvice.java
@@ -2,6 +2,7 @@ package wooteco.subway.line.ui;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,12 @@ import wooteco.subway.line.exception.LineDomainException;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @RestControllerAdvice(assignableTypes = LineController.class)
 public class LineControllerAdvice {
+    @ExceptionHandler(DuplicateKeyException.class)
+    public ResponseEntity<ErrorResponse> handleDuplicateKeyException(DuplicateKeyException e) {
+        return ResponseEntity.badRequest()
+                .body(new ErrorResponse("이미 존재하는 노선 이름 혹은 색상입니다."));
+    }
+
     @ExceptionHandler(LineDomainException.class)
     public ResponseEntity<ErrorResponse> handleLineDomainException(LineDomainException e) {
         e.printStackTrace();
