181a931589f9daeee5b437f9be7c54906bc3dd3d
Eugine Park <pkeugine@gmail.com>
2021-05-26 20:10:56 +0900

    fix: preflight 설정 추가 (CORS)

--- src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
+++ src/main/java/wooteco/subway/auth/ui/LoginInterceptor.java
@@ -16,6 +16,9 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
         if ("GET".equalsIgnoreCase(request.getMethod())) {
             return true;
         }
