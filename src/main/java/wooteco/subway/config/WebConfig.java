ec5d3680cdc9ae2343c4e41fe31ab9cf4ee38c8f
kimjunseo <air.junseo@gmail.com>
2021-05-26 15:05:53 +0900

    refactor : nginx 인스턴스 ip로 접근하는 요청만 허용해주기

--- src/main/java/wooteco/subway/config/WebConfig.java
+++ src/main/java/wooteco/subway/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH")
+                .allowedOrigins("https://192.168.0.78/*")
                 .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
