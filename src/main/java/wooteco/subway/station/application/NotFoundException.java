faa85b995f46c94d5aa00f40e166f6d6a78918a3
Eugine Park <pkeugine@gmail.com>
2021-05-27 00:03:41 +0900

    feat: NotFoundException 추가

---
+++ src/main/java/wooteco/subway/station/application/NotFoundException.java
@@ -0,0 +1,16 @@
+package wooteco.subway.station.application;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
