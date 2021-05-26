0ab6df2d8241f17668aeed12f780bcee886defa9
JinYoung Park <basejin0810@naver.com>
2021-05-26 14:30:50 +0900

    feat(StationRequest): 지하철역 입력 유효성 검증 로직 추가

--- src/main/java/wooteco/subway/station/dto/StationRequest.java
+++ src/main/java/wooteco/subway/station/dto/StationRequest.java
@@ -1,9 +1,13 @@
 package wooteco.subway.station.dto;
 
+import javax.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 import wooteco.subway.station.domain.Station;
 
 public class StationRequest {
 
+    @NotBlank(message = "이름에 공백이 있을 수 없습니다.")
+    @Length(min = 2, max = 10, message = "역 이름은 2글자 이상 10글자 이하여야합니다.")
     private String name;
 
     public StationRequest() {
