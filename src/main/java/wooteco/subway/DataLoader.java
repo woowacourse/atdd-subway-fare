37bb55392a69d29d76d58b0b19f0e63171aec34d
Seed <hsik0225@gmail.com>
2021-05-26 11:14:19 +0900

    refactor : 사용하지 않는 ExceptionHandler 제거

--- src/main/java/wooteco/subway/DataLoader.java
+++ src/main/java/wooteco/subway/DataLoader.java
@@ -3,6 +3,7 @@ package wooteco.subway;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
+
 import wooteco.subway.line.dao.LineDao;
 import wooteco.subway.line.dao.SectionDao;
 import wooteco.subway.line.domain.Line;
@@ -29,7 +30,6 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        stationDao.findById(1L);
         Station 강남역 = stationDao.insert(new Station("강남역"));
         Station 판교역 = stationDao.insert(new Station("판교역"));
         Station 정자역 = stationDao.insert(new Station("정자역"));
--- src/main/java/wooteco/subway/station/ui/StationController.java
+++ src/main/java/wooteco/subway/station/ui/StationController.java
@@ -1,15 +1,15 @@
 package wooteco.subway.station.ui;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import wooteco.subway.station.application.StationService;
 import wooteco.subway.station.dto.StationRequest;
 import wooteco.subway.station.dto.StationResponse;
-
-import java.net.URI;
-import java.sql.SQLException;
-import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -35,10 +35,5 @@ public class StationController {
     public ResponseEntity deleteStation(@PathVariable Long id) {
         stationService.deleteStationById(id);
         return ResponseEntity.noContent().build();
-    }
-
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity handleSQLException() {
-        return ResponseEntity.badRequest().build();
     }
 }
