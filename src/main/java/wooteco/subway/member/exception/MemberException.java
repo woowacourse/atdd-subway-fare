046b4cb0794f51cada2da6e1be0f80b0e402c207
pika96 <52442949+pika96@users.noreply.github.com>
2021-05-26 14:31:41 +0900

    feat: member 에러 메시지 변경

--- src/main/java/wooteco/subway/member/exception/MemberException.java
+++ src/main/java/wooteco/subway/member/exception/MemberException.java
@@ -6,9 +6,9 @@ import wooteco.subway.exception.SubwayException;
 public enum MemberException implements SubwayException {
 
     DUPLICATED_EMAIL_EXCEPTION("중복된 이메일입니다.", HttpStatus.BAD_REQUEST.value()),
-    INVALID_EMAIL("유효하지 않은 이메일입니다.", HttpStatus.BAD_REQUEST.value()),
-    INVALID_PASSWORD("유효하지 않은 비밀번호입니다.", HttpStatus.BAD_REQUEST.value()),
-    INVALID_AGE("유효하지 않은 나이입니다.", HttpStatus.BAD_REQUEST.value());
+    INVALID_EMAIL("잘못된 이메일입니다.", HttpStatus.BAD_REQUEST.value()),
+    INVALID_PASSWORD("잘못된 비밀번호입니다.", HttpStatus.BAD_REQUEST.value()),
+    INVALID_AGE("잘못된 나이입니다.", HttpStatus.BAD_REQUEST.value());
 
     private final String message;
     private final int status;
