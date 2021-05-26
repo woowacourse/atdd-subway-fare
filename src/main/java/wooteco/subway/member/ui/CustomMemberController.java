b95c454df2eadefece235ba0451b2b7f98fb85a6
hyeon9mak <jinha3507@gmail.com>
2021-05-26 16:54:52 +0900

    feat: api 주소 사용을 위해 컨트롤러 추가

---
+++ src/main/java/wooteco/subway/member/ui/CustomMemberController.java
@@ -0,0 +1,52 @@
+package wooteco.subway.member.ui;
+
+import java.net.URI;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wooteco.subway.auth.domain.AuthenticationPrincipal;
+import wooteco.subway.member.application.MemberService;
+import wooteco.subway.member.domain.LoginMember;
+import wooteco.subway.member.dto.MemberRequest;
+import wooteco.subway.member.dto.MemberResponse;
+
+@RestController
+@RequestMapping("/api/members")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CustomMemberController {
+    private MemberService memberService;
+
+    public CustomMemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity createMember(@RequestBody MemberRequest request) {
+        MemberResponse member = memberService.createMember(request);
+        return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponse> findMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+        MemberResponse member = memberService.findMember(loginMember);
+        return ResponseEntity.ok().body(member);
+    }
+
+    @PutMapping("/me")
+    public ResponseEntity<MemberResponse> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
+        memberService.updateMember(loginMember, param);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/me")
+    public ResponseEntity<MemberResponse> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+        memberService.deleteMember(loginMember);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/wooteco/subway/station/ui/CustomStationController.java
@@ -0,0 +1,45 @@
+package wooteco.subway.station.ui;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import wooteco.subway.station.application.StationService;
+import wooteco.subway.station.dto.StationRequest;
+import wooteco.subway.station.dto.StationResponse;
+
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stations")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CustomStationController {
+    private StationService stationService;
+
+    public CustomStationController(StationService stationService) {
+        this.stationService = stationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
+        StationResponse station = stationService.saveStation(stationRequest);
+        return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StationResponse>> showStations() {
+        return ResponseEntity.ok().body(stationService.findAllStationResponses());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteStation(@PathVariable Long id) {
+        stationService.deleteStationById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity handleSQLException() {
+        return ResponseEntity.badRequest().build();
+    }
+}
