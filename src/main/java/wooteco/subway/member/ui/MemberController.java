e264b18143a02dbdda8346034b7bfaabca7911f9
seunghyun <ggyool@naver.com>
2021-05-26 10:35:06 +0900

    refactor: ResponseEntity 반환타입 정리

--- src/main/java/wooteco/subway/member/ui/MemberController.java
+++ src/main/java/wooteco/subway/member/ui/MemberController.java
@@ -21,7 +21,7 @@ public class MemberController {
     }
 
     @PostMapping("/members")
-    public ResponseEntity createMember(@RequestBody MemberRequest request) {
+    public ResponseEntity<Void> createMember(@RequestBody MemberRequest request) {
         MemberResponse member = memberService.createMember(request);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).build();
     }
@@ -33,19 +33,19 @@ public class MemberController {
     }
 
     @PutMapping("/members/me")
-    public ResponseEntity<MemberResponse> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
+    public ResponseEntity<Void> updateMemberOfMine(@AuthenticationPrincipal LoginMember loginMember, @RequestBody MemberRequest param) {
         memberService.updateMember(loginMember, param);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/members/me")
-    public ResponseEntity<MemberResponse> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
+    public ResponseEntity<Void> deleteMemberOfMine(@AuthenticationPrincipal LoginMember loginMember) {
         memberService.deleteMember(loginMember);
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping("/members/email-check")
-    public ResponseEntity<MemberResponse> checkEmail(@RequestBody EmailRequest emailRequest) {
+    public ResponseEntity<Void> checkEmail(@RequestBody EmailRequest emailRequest) {
         memberService.checkEmail(emailRequest.getEmail());
         return ResponseEntity.ok().build();
     }
