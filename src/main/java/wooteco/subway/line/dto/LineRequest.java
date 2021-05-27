86a9d020d62f925c2a6f5339cd89a96f457f973e
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-28 00:57:01 +0900

    feat(LineRequest): LineRequest에 대해 Spring Validation 적용

--- src/main/java/wooteco/subway/line/dto/LineRequest.java
+++ src/main/java/wooteco/subway/line/dto/LineRequest.java
@@ -1,11 +1,32 @@
 package wooteco.subway.line.dto;
 
+import org.hibernate.validator.constraints.Length;
+import wooteco.subway.line.exception.InvalidLineColorException;
+import wooteco.subway.line.exception.InvalidLineNameException;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 public class LineRequest {
+    @NotBlank(message = InvalidLineNameException.ERROR_MESSAGE)
+    @Length(min = 2, message = InvalidLineNameException.ERROR_MESSAGE)
+    @Pattern(regexp = "^[가-힣|0-9]*$", message = InvalidLineNameException.ERROR_MESSAGE)
     private String name;
+
+    @NotBlank(message = InvalidLineColorException.ERROR_MESSAGE)
     private String color;
+
+    @NotNull
     private Long upStationId;
+
+    @NotNull
     private Long downStationId;
+
+    @NotNull
     private int distance;
+
+    @NotNull
     private int extraFare;
 
     public LineRequest() {
