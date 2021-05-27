757668a550ec46a1dfe7a48190279a55b4d04dd7
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-27 12:24:39 +0900

    feat: MemberDao 테스트 추가

--- src/main/java/wooteco/subway/member/domain/Member.java
+++ src/main/java/wooteco/subway/member/domain/Member.java
@@ -4,6 +4,8 @@ import org.apache.commons.lang3.StringUtils;
 import wooteco.subway.auth.application.AuthorizationException;
 import wooteco.subway.member.exception.MismatchIdPasswordException;
 
+import java.util.Objects;
+
 public class Member {
     private Long id;
     private String email;
@@ -52,5 +54,18 @@ public class Member {
         if (!StringUtils.equals(this.password, password)) {
             throw new MismatchIdPasswordException();
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
