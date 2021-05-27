a94b633a55c5d9c94d0ddf05362b04987288e308
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-27 20:41:59 +0900

    refactor(DuplicateStationNameException): DuplicateKeyException을 상속받도록 변경

--- src/main/java/wooteco/subway/station/exception/DuplicateStationNameException.java
+++ src/main/java/wooteco/subway/station/exception/DuplicateStationNameException.java
@@ -1,6 +1,8 @@
 package wooteco.subway.station.exception;
 
-public class DuplicateStationNameException extends IllegalArgumentException {
+import org.springframework.dao.DuplicateKeyException;
+
+public class DuplicateStationNameException extends DuplicateKeyException {
     private static final String ERROR_MESSAGE = "이미 등록된 역입니다.";
 
     public DuplicateStationNameException() {
