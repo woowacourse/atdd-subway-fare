9dff119df3539fa796d64ca3e70925bbf122b025
TaeJeong Kim <xowjd41@naver.com>
2021-05-27 16:44:25 +0900

    refactor(MemberService.class): email 로직 변경

--- src/main/java/wooteco/subway/member/application/MemberService.java
+++ src/main/java/wooteco/subway/member/application/MemberService.java
@@ -26,21 +26,24 @@ public class MemberService {
     }
 
     public MemberResponse findMember(LoginMember loginMember) {
-        if (!memberDao.existsEmail(loginMember.getEmail())) {
-            throw new AuthorizationException("존재하지 않는 이메일입니다.");
-        }
-
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = findByEmail(loginMember);
         return MemberResponse.of(member);
     }
 
     public void updateMember(LoginMember loginMember, MemberRequest memberRequest) {
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = findByEmail(loginMember);
         memberDao.update(new Member(member.getId(), memberRequest.getEmail(), memberRequest.getPassword(), memberRequest.getAge()));
     }
 
     public void deleteMember(LoginMember loginMember) {
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = findByEmail(loginMember);
         memberDao.deleteById(member.getId());
+    }
+
+    private Member findByEmail(LoginMember loginMember) {
+        if (!memberDao.existsEmail(loginMember.getEmail())) {
+            throw new AuthorizationException("존재하지 않는 이메일입니다.");
+        }
+        return memberDao.findByEmail(loginMember.getEmail());
     }
 }
