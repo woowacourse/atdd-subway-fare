ae636cc949c2c446bf25c9a8cd51f951c3964940
Minsung <sns2831@gmail.com>
2021-05-27 16:49:46 +0900

    fix: sql문 Line -> line 변경

--- src/main/java/wooteco/subway/line/infrastructure/dao/LineDao.java
+++ src/main/java/wooteco/subway/line/infrastructure/dao/LineDao.java
@@ -108,7 +108,7 @@ public class LineDao {
     }
 
     public void deleteById(Long id) {
-        jdbcTemplate.update("delete from Line where id = ?", id);
+        jdbcTemplate.update("delete from line where id = ?", id);
     }
 
 }
