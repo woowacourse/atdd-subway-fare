56aaa50a69d7075338fc338a9d7cbe5d16495612
lns13301 <rkdrbgks22@ks.ac.kr>
2021-05-27 22:22:50 +0900

    feat: 요청에 대한 Valid 추가

--- src/main/java/wooteco/subway/station/ui/StationController.java
+++ src/main/java/wooteco/subway/station/ui/StationController.java
@@ -2,11 +2,15 @@ package wooteco.subway.station.ui;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import wooteco.subway.station.application.StationService;
 import wooteco.subway.station.dto.StationRequest;
 import wooteco.subway.station.dto.StationResponse;
+import wooteco.subway.validate.LineValidator;
+import wooteco.subway.validate.StationValidator;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
@@ -19,8 +23,13 @@ public class StationController {
         this.stationService = stationService;
     }
 
+    @InitBinder("stationRequest")
+    private void initBind(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(new StationValidator());
+    }
+
     @PostMapping("/stations")
-    public ResponseEntity<StationResponse> createStation(@RequestBody StationRequest stationRequest) {
+    public ResponseEntity<StationResponse> createStation(@RequestBody @Valid StationRequest stationRequest) {
         StationResponse station = stationService.saveStation(stationRequest);
         return ResponseEntity.created(URI.create("/stations/" + station.getId())).body(station);
     }
@@ -31,7 +40,7 @@ public class StationController {
     }
 
     @PutMapping("/stations/{id}")
-    public ResponseEntity<Void> updateStation(@PathVariable Long id, @RequestBody StationRequest stationRequest) {
+    public ResponseEntity<Void> updateStation(@PathVariable Long id, @RequestBody @Valid StationRequest stationRequest) {
         stationService.updateStation(id, stationRequest);
         return ResponseEntity.ok().build();
     }
---
+++ src/main/java/wooteco/subway/validate/MemberValidator.java
@@ -0,0 +1,41 @@
+package wooteco.subway.validate;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import wooteco.subway.exception.AgeInvalidBoundaryException;
+import wooteco.subway.exception.EmailInvalidBoundaryException;
+import wooteco.subway.exception.PasswordInvalidBoundaryException;
+import wooteco.subway.exception.UnsupportedCharacterException;
+import wooteco.subway.member.dto.MemberRequest;
+
+import java.util.regex.Pattern;
+
+public class MemberValidator implements Validator {
+    private static final String PATTERN = "^[a-zA-Z0-9]*$";
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return MemberRequest.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        MemberRequest memberRequest = (MemberRequest) target;
+
+        if (Pattern.matches(PATTERN, memberRequest.getEmail())) {
+            throw new UnsupportedCharacterException();
+        }
+
+        if (memberRequest.getEmail().length() > 30) {
+            throw new EmailInvalidBoundaryException();
+        }
+
+        if (memberRequest.getPassword().length() < 4 && memberRequest.getPassword().length() > 20) {
+            throw new PasswordInvalidBoundaryException();
+        }
+
+        if (memberRequest.getAge() < 0 && memberRequest.getAge() > 200) {
+            throw new AgeInvalidBoundaryException();
+        }
+    }
+}
