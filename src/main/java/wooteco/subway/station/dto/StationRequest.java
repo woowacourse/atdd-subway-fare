dbb0b1dcd9f2d5b29db49f73e30b6c20b4dec8b1
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-27 14:24:39 +0900

    refactor: StationRequest 검증 추가

--- src/main/java/wooteco/subway/station/dto/StationRequest.java
+++ src/main/java/wooteco/subway/station/dto/StationRequest.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 public class StationRequest {
-    @NotBlank
+    @NotBlank(message = "역 이름은 비어있을 수 없습니다.")
     @Pattern(regexp = "^[가-힣0-9]{2,20}$", message = "역 이름은 2~20자 이하의 한글/숫자만 가능합니다")
     private String name;
 
