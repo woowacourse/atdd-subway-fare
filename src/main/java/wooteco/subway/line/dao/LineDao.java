dab87034b6726317a80598ee94aead4be861adb4
JinYoung Park <basejin0810@naver.com>
2021-05-27 11:24:47 +0900

    fix(LineDao): 삭제 관련 SQL문 수정

--- src/main/java/wooteco/subway/line/dao/LineDao.java
+++ src/main/java/wooteco/subway/line/dao/LineDao.java
@@ -111,7 +111,7 @@ public class LineDao {
     }
 
     public void deleteById(Long id) {
-        jdbcTemplate.update("delete from Line where id = ?", id);
+        jdbcTemplate.update("delete from LINE where id = ?", id);
     }
 
     public boolean existsById(Long id) {
