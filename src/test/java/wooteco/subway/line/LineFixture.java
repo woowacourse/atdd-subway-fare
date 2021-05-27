d51c51298c10adfd543687e595a78bae3808199f
kimjunseo <air.junseo@gmail.com>
2021-05-27 10:46:57 +0900

    feat : 경로에 포함된 노선의 추가 요금 계산 로직 구현

--- src/test/java/wooteco/subway/line/LineFixture.java
+++ src/test/java/wooteco/subway/line/LineFixture.java
@@ -3,7 +3,7 @@ package wooteco.subway.line;
 import wooteco.subway.line.domain.Line;
 
 public class LineFixture {
-    public static Line 신분당선 = new Line(1L, "신분당선", "yellow");
-    public static Line 이호선 = new Line(2L, "2호선", "green");
-    public static Line 사호선 = new Line(3L, "4호선", "sky");
+    public static Line 신분당선 = new Line(1L, "신분당선", "yellow", 1000);
+    public static Line 이호선 = new Line(2L, "2호선", "green", 1500);
+    public static Line 사호선 = new Line(3L, "4호선", "sky", 2000);
 }
--- src/test/java/wooteco/subway/path/domain/SubwayPathTest.java
+++ src/test/java/wooteco/subway/path/domain/SubwayPathTest.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static wooteco.subway.line.LineFixture.이호선;
+import static wooteco.subway.line.LineFixture.*;
 import static wooteco.subway.line.SectionFixture.*;
 
 class SubwayPathTest {
@@ -38,7 +38,7 @@ class SubwayPathTest {
         int fare = subwayPath.calculateFare();
 
         // then
-        assertThat(fare).isEqualTo(2150);
+        assertThat(fare).isEqualTo(2150 + 1500);
     }
 
     @DisplayName("요금 계산 - 10km ~ 50km")
@@ -62,7 +62,7 @@ class SubwayPathTest {
         int fare = subwayPath.calculateFare();
 
         // then
-        assertThat(fare).isEqualTo(2050);
+        assertThat(fare).isEqualTo(2050 + 1500);
     }
 
     @DisplayName("요금 계산 - 10km 이내")
@@ -82,6 +82,34 @@ class SubwayPathTest {
         int fare = subwayPath.calculateFare();
 
         // then
-        assertThat(fare).isEqualTo(1250);
+        assertThat(fare).isEqualTo(1250 + 1500);
+    }
+
+    @DisplayName("환승 요금 계산")
+    @Test
+    void TransferFare() {
+        // given
+        List<SectionEdge> sectionEdges = Arrays.asList(
+                new SectionEdge(
+                        왕십리_잠실_거리22,
+                        신분당선
+                ),
+                new SectionEdge(
+                        잠실_강남_거리25,
+                        이호선
+                ),
+                new SectionEdge(
+                        강남_구의_거리10,
+                        사호선
+                )
+        );
+
+        SubwayPath subwayPath = new SubwayPath(sectionEdges, Collections.emptyList());
+
+        // when
+        int fare = subwayPath.calculateFare();
+
+        // then
+        assertThat(fare).isEqualTo(4150);
     }
 }
