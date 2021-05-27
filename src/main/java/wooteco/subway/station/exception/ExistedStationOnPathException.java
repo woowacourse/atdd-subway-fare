a99c2f2aacd78d3334b24afc75c7e06792327629
YeongBeom Heo <hybeom@gmail.com>
2021-05-27 18:44:02 +0900

    fix(ExistedStationOnPathException) : 노선 등록된 역 삭제 예외 메세지 변경

--- src/main/java/wooteco/subway/station/exception/ExistedStationOnPathException.java
+++ src/main/java/wooteco/subway/station/exception/ExistedStationOnPathException.java
@@ -3,6 +3,6 @@ package wooteco.subway.station.exception;
 public class ExistedStationOnPathException extends RuntimeException {
 
     public ExistedStationOnPathException() {
-        super("구간 위에 역이 존재를 합니다.");
+        super("노선에 등록된 역은 삭제할 수 없습니다.");
     }
 }
