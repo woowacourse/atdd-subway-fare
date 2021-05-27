e0db0d69a1058a9497641036b821181056c502b2
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-27 20:40:41 +0900

    refactor(MemberRequest): 회원가입 요청에 대한 Spring validation 구현

--- src/main/java/wooteco/subway/member/dto/MemberRequest.java
+++ src/main/java/wooteco/subway/member/dto/MemberRequest.java
@@ -2,9 +2,23 @@ package wooteco.subway.member.dto;
 
 import wooteco.subway.member.domain.Member;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
 public class MemberRequest {
+    private static final String INVALID_MEMBER_REQUEST_MESSAGE = "양식에 맞지 않는 회원가입 요청입니다.";
+
+    @NotBlank(message = INVALID_MEMBER_REQUEST_MESSAGE)
+    @Email(message = INVALID_MEMBER_REQUEST_MESSAGE)
     private String email;
+
+    @NotBlank(message = INVALID_MEMBER_REQUEST_MESSAGE)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = INVALID_MEMBER_REQUEST_MESSAGE)
     private String password;
+
+    @Positive(message = INVALID_MEMBER_REQUEST_MESSAGE)
     private Integer age;
 
     public MemberRequest() {
