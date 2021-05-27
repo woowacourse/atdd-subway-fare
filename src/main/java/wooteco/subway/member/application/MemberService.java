1437290eea3ad7b69f64f657def0577b6ef33fc9
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-05-27 20:38:41 +0900

    refactor(MemberService): Optional<Member> 유무에 따른 예외 처리 로직 추가

--- src/main/java/wooteco/subway/member/application/MemberService.java
+++ src/main/java/wooteco/subway/member/application/MemberService.java
@@ -1,11 +1,15 @@
 package wooteco.subway.member.application;
 
 import org.springframework.stereotype.Service;
+import wooteco.subway.auth.application.AuthorizationException;
 import wooteco.subway.member.dao.MemberDao;
 import wooteco.subway.member.domain.LoginMember;
 import wooteco.subway.member.domain.Member;
 import wooteco.subway.member.dto.MemberRequest;
 import wooteco.subway.member.dto.MemberResponse;
+import wooteco.subway.member.exception.DuplicateEmailException;
+
+import java.util.Optional;
 
 @Service
 public class MemberService {
@@ -16,22 +20,29 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest request) {
+        final Optional<Member> memberByEmail = memberDao.findByEmail(request.getEmail());
+        if (memberByEmail.isPresent()) {
+            throw new DuplicateEmailException();
+        }
         Member member = memberDao.insert(request.toMember());
         return MemberResponse.of(member);
     }
 
     public MemberResponse findMember(LoginMember loginMember) {
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = memberDao.findByEmail(loginMember.getEmail())
+                .orElseThrow(AuthorizationException::new);
         return MemberResponse.of(member);
     }
 
     public void updateMember(LoginMember loginMember, MemberRequest memberRequest) {
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = memberDao.findByEmail(loginMember.getEmail())
+                .orElseThrow(AuthorizationException::new);
         memberDao.update(new Member(member.getId(), memberRequest.getEmail(), memberRequest.getPassword(), memberRequest.getAge()));
     }
 
     public void deleteMember(LoginMember loginMember) {
-        Member member = memberDao.findByEmail(loginMember.getEmail());
+        Member member = memberDao.findByEmail(loginMember.getEmail())
+                .orElseThrow(AuthorizationException::new);
         memberDao.deleteById(member.getId());
     }
 }
