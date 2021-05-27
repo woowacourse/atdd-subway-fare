91ad10e0c279bc19a5519c3b60dfcaef4a503399
Seed <hsik0225@gmail.com>
2021-05-27 14:20:27 +0900

    feat : 역이 어느 노선 안에 포함되어 있는지 필터링 하는 기능 추가

--- src/main/java/wooteco/subway/line/dto/SimpleLineResponse.java
+++ src/main/java/wooteco/subway/line/dto/SimpleLineResponse.java
@@ -1,20 +1,30 @@
 package wooteco.subway.line.dto;
 
+import wooteco.subway.line.domain.Line;
+
 public class SimpleLineResponse {
     private Long id;
     private String name;
     private String color;
+
+    public SimpleLineResponse(Line line) {
+        this(line.getId(), line.getName(), line.getColor());
+    }
+
     public SimpleLineResponse(Long id, String name, String color) {
         this.id = id;
         this.name = name;
         this.color = color;
     }
+
     public Long getId() {
         return id;
     }
+
     public String getName() {
         return name;
     }
+
     public String getColor() {
         return color;
     }
