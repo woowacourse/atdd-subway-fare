aec97f46682810bed705ce604842f4dc89f49672
cjy17 <cjy1705@naver.com>
2021-05-28 22:45:16 +0900

    feat : 토큰 검증 실패 메시지 추가

--- src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/wooteco/subway/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -27,7 +27,7 @@ public class AuthenticationPrincipalArgumentResolver extends AuthenticationArgum
     @Override
     protected LoginMember validMember(LoginMember member) {
         if (member.getId() == null) {
-            throw new AuthorizationException("");
+            throw new AuthorizationException("토큰 검증에 실패했습니다.");
         }
         return member;
     }
