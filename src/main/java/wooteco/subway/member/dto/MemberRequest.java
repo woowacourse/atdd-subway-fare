74bbbe26f47c601faa67027e9e855ae90cdd4fa1
sakjung <ssangyu123@gmail.com>
2021-05-29 00:12:51 +0900

    feat: swagger models 부분에 request dto의 validation 사항도 나오게끔 하는 기능 추가

--- src/main/java/wooteco/subway/member/dto/MemberRequest.java
+++ src/main/java/wooteco/subway/member/dto/MemberRequest.java
@@ -8,9 +8,11 @@ import javax.validation.constraints.*;
 public class MemberRequest {
     @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
+
     @NotBlank(message = "비밀번호는 공백일 수 없습니다")
     @Size(min = 4, max = 20, message = "비밀번호는 4자리 에서 20자리 사이여야 합니다.")
     private String password;
+
     @Max(value = 200, message = "나이는 200 초과 일 수 없습니다.")
     @Min(value = 1, message = "나이는 1 미만일 수 없습니다.")
     private Integer age;
