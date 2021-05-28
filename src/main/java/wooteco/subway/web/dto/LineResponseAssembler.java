ce8aa7a2b0a7b2990df3dd752115cdcefd9fb9be
bomin <bomin_93@naver.com>
2021-05-28 19:22:06 +0900

    refactor: 프론트 요구사항에 response 맞춤

--- src/main/java/wooteco/subway/web/dto/LineResponseAssembler.java
+++ src/main/java/wooteco/subway/web/dto/LineResponseAssembler.java
@@ -14,20 +14,16 @@ public class LineResponseAssembler {
         final ArrayList<StationWithDistanceResponse> stationResponses = new ArrayList<>();
         final List<Section> sections = line.sortedSections();
         for (int i = 0; i < sections.size(); i++) {
-            if (i == sections.size() - 1) {
-                final Section lastSection = sections.get(i);
-                final Station upStation = lastSection.getUpStation();
-                final Station downStation = lastSection.getDownStation();
-                stationResponses.add(new StationWithDistanceResponse(upStation.getId(),
-                    upStation.getName(), lastSection.getDistance()));
-                stationResponses.add(new StationWithDistanceResponse(downStation.getId(),
-                    downStation.getName()));
-                break;
-            }
             final Section section = sections.get(i);
             final Station upStation = section.getUpStation();
             stationResponses.add(new StationWithDistanceResponse(upStation.getId(),
                 upStation.getName(), section.getDistance()));
+
+            if (i == sections.size() - 1) {
+                final Station downStation = section.getDownStation();
+                stationResponses.add(new StationWithDistanceResponse(downStation.getId(),
+                    downStation.getName()));
+            }
         }
         return new LineResponse(line.getId(), line.getName(), line.getColor(), stationResponses);
     }
