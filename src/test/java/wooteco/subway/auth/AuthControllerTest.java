de315da29b3b984b826d01ee8f052bb67f527a60
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-05-29 18:19:05 +0900

    feat: AuthController 문서화 작업

---
+++ src/test/java/wooteco/subway/auth/AuthControllerTest.java
@@ -0,0 +1,61 @@
+package wooteco.subway.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import wooteco.subway.auth.application.AuthService;
+import wooteco.subway.auth.dto.TokenRequest;
+import wooteco.subway.auth.dto.TokenResponse;
+import wooteco.subway.auth.ui.AuthController;
+import wooteco.subway.auth.ui.LoginInterceptor;
+import wooteco.subway.line.application.LineService;
+import wooteco.subway.line.dto.LineRequest;
+import wooteco.subway.line.dto.LineResponse;
+import wooteco.subway.line.ui.LineController;
+import wooteco.subway.station.dto.StationResponse;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = AuthController.class)
+@ActiveProfiles("test")
+@AutoConfigureRestDocs
+public class AuthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private LoginInterceptor loginInterceptor;
+    @MockBean
+    private AuthService authService;
+
+    @Test
+    @DisplayName("로그인")
+    public void login() throws Exception {
+        TokenRequest tokenRequest = new TokenRequest("email@email.com", "password");
+        given(authService.login(tokenRequest)).willReturn(new TokenResponse("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiaWF0IjoxNjIyMjAzOTU5LCJleHAiOjE2MjIyMDc1NTl9.Ft7GLR6K9osybNLVScma-j_t48WS4ZTKsvsjUFKpA78"));
+
+        mockMvc.perform(post("/api/lines")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(tokenRequest)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andDo(document("login"));
+    }
+}
