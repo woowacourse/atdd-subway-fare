20027ddb44a49e6ab88a3631ca2d7d181598dc77
xrabcde <xrabcde@gmail.com>
2021-05-29 20:56:38 +0900

    refactor: 테스트코드 리팩터링

--- src/test/java/wooteco/subway/station/StationFixture.java
+++ src/test/java/wooteco/subway/station/StationFixture.java
@@ -7,6 +7,4 @@ public class StationFixture {
     public static Station 잠실역 = new Station(2L, "잠실역");
     public static Station 강남역 = new Station(3L, "강남역");
     public static Station 구의역 = new Station(4L, "구의역");
-    public static Station 건대입구역 = new Station(5L, "건대입구");
-    public static Station 한양대역 = new Station(6L, "한양대");
 }
