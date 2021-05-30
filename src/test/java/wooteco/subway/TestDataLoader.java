4b8cf38757f42d5a333e9e9cb477584daf2cd488
sihyung92 <fjzjqhdl@gmail.com>
2021-05-30 10:20:00 +0900

    refactor : test data loader의 데이터를 호출시 생성하는 방식으로 변경

--- src/test/java/wooteco/subway/TestDataLoader.java
+++ src/test/java/wooteco/subway/TestDataLoader.java
@@ -7,91 +7,58 @@ import wooteco.subway.station.domain.Station;
 
 public class TestDataLoader {
 
-    Station 강남역;
-    Station 판교역;
-    Station 정자역;
-    Station 역삼역;
-    Station 잠실역;
-    Station 산본역;
-    Station 금정역;
-    Station 범계역;
-
-    Line 신분당선;
-    Line 이호선;
-    Line 사호선;
-
-    LoginMember 청소년;
-    LoginMember 어린이;
-    LoginMember 일반;
-    LoginMember 게스트;
-
-    public TestDataLoader() {
-        강남역 = new Station(1L, "강남역");
-        판교역 = new Station(2L, "판교역");
-        정자역 = new Station(3L, "정자역");
-        역삼역 = new Station(4L, "역삼역");
-        잠실역 = new Station(5L, "잠실역");
-        산본역 = new Station(6L, "산본역");
-        금정역 = new Station(7L, "금정역");
-        범계역 = new Station(8L, "범계역");
-
-        int zeroExtraFare = 0;
-        신분당선 = new Line(1L, "신분당선", "red lighten-1", zeroExtraFare);
-        신분당선.addSection(new Section(강남역, 판교역, 5));
-        신분당선.addSection(new Section(판교역, 정자역, 5));
-
-        이호선 = new Line(2L, "2호선", "green lighten-1", zeroExtraFare);
-        이호선.addSection(new Section(강남역, 역삼역, 25));
-        이호선.addSection(new Section(역삼역, 잠실역, 25));
-
-        사호선 = new Line(3L, "4호선", "blue lighten-1", zeroExtraFare);
-        사호선.addSection(new Section(산본역, 금정역, 50));
-        사호선.addSection(new Section(금정역, 범계역, 50));
-
-
-    }
+    private int zeroExtraFare = 0;
 
     public Line 신분당선() {
+        Line 신분당선 = new Line(1L, "신분당선", "red lighten-1", zeroExtraFare);
+        신분당선.addSection(new Section(강남역(), 판교역(), 5));
+        신분당선.addSection(new Section(판교역(), 정자역(), 5));
         return 신분당선;
     }
 
     public Line 이호선() {
+        Line 이호선 = new Line(2L, "2호선", "green lighten-1", zeroExtraFare);
+        이호선.addSection(new Section(강남역(), 역삼역(), 25));
+        이호선.addSection(new Section(역삼역(), 잠실역(), 25));
         return 이호선;
     }
 
     public Line 사호선() {
+        Line 사호선 = new Line(3L, "4호선", "blue lighten-1", zeroExtraFare);
+        사호선.addSection(new Section(산본역(), 금정역(), 50));
+        사호선.addSection(new Section(금정역(), 범계역(), 50));
         return 사호선;
     }
 
     public Station 강남역() {
-        return 강남역;
+        return new Station(1L, "강남역");
     }
 
     public Station 판교역() {
-        return 판교역;
+        return new Station(2L, "판교역");
     }
 
     public Station 정자역() {
-        return 정자역;
+        return new Station(3L, "정자역");
     }
 
     public Station 역삼역() {
-        return 역삼역;
+        return new Station(4L, "역삼역");
     }
 
     public Station 잠실역() {
-        return 잠실역;
+        return new Station(5L, "잠실역");
     }
 
     public Station 산본역() {
-        return 산본역;
+        return new Station(6L, "산본역");
     }
 
     public Station 금정역() {
-        return 금정역;
+        return new Station(7L, "금정역");
     }
 
     public Station 범계역() {
-        return 범계역;
+        return new Station(8L, "범계역");
     }
 }
