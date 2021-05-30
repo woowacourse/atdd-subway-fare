679d4391748cd1b88d36f04dd1fd9ea60b9958b1
sihyung92 <fjzjqhdl@gmail.com>
2021-05-30 09:37:38 +0900

    test : guest에 대한 요금 계산 테스트 추가

--- src/test/java/wooteco/subway/member/domain/LoginMemberTest.java
+++ src/test/java/wooteco/subway/member/domain/LoginMemberTest.java
@@ -14,12 +14,14 @@ class LoginMemberTest {
         LoginMember 청소년 = new LoginMember(1L, "yjksw@github.com", 13);
         LoginMember 어린이 = new LoginMember(2L, "fjzjqhdl@gmail.com", 6);
         LoginMember 일반인 = new LoginMember(3L, "nabom@gmail.com", 30);
+        LoginMember 게스트 = new LoginMember();
 
         Fare baseFare = new Fare(1250);
 
         assertThat(청소년.calculateFare(baseFare)).isEqualTo(new Fare(720)); // 350원 공제 후 80%
         assertThat(어린이.calculateFare(baseFare)).isEqualTo(new Fare(450)); // 350원 공제 후 50%
         assertThat(일반인.calculateFare(baseFare)).isEqualTo(new Fare(1250)); // 그대로
+        assertThat(게스트.calculateFare(baseFare)).isEqualTo(new Fare(1250)); // 그대로
     }
 
 }
