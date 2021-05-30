f36baf2b3afac7ad1358d15516ffe00f94f6ad19
younghoonkwon <younghoon.kwon91@gmail.com>
2021-05-31 03:19:38 +0900

    refactor: 상수 대문자화

--- src/main/java/wooteco/subway/member/exception/EmailNotFoundRuntimeException.java
+++ src/main/java/wooteco/subway/member/exception/EmailNotFoundRuntimeException.java
@@ -3,9 +3,9 @@ package wooteco.subway.member.exception;
 import wooteco.subway.exception.AuthorizationException;
 
 public class EmailNotFoundRuntimeException extends AuthorizationException {
-    private static final String errorMessage = "존재하지 않는 계정입니다";
+    private static final String ERROR_MESSAGE = "존재하지 않는 계정입니다";
 
     public EmailNotFoundRuntimeException() {
-        super(errorMessage);
+        super(ERROR_MESSAGE);
     }
 }
