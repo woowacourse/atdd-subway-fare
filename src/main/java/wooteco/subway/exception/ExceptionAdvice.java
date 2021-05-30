b0b75a5d533c8c64f2d7fa9fd3bf0442a5897b87
hyeon9mak <jinha3507@gmail.com>
2021-05-30 15:56:21 +0900

    refactor: 회원정보 입력 관련 @Valid 어노테이션 추가

--- src/main/java/wooteco/subway/exception/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/exception/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package wooteco.subway.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +18,13 @@ public class ExceptionAdvice {
 
         return ResponseEntity.status(e.status())
             .body(new ExceptionResponse(e.error()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> exceptionResponse(MethodArgumentNotValidException e) {
+        LOGGER.error(e.getMessage());
+
+        return ResponseEntity.badRequest()
+            .body(new ExceptionResponse(e.getMessage()));
     }
 }
--- src/main/java/wooteco/subway/member/dto/MemberRequest.java
+++ src/main/java/wooteco/subway/member/dto/MemberRequest.java
@@ -1,10 +1,20 @@
 package wooteco.subway.member.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import wooteco.subway.member.domain.Member;
 
 public class MemberRequest {
+
+    @NotNull(message = "INVALID_EMAIL")
+    @Email(message = "INVALID_EMAIL")
     private String email;
+    @NotBlank(message = "INVALID_PASSWORD")
     private String password;
+    @NotNull(message = "INVALID_AGE")
+    @PositiveOrZero(message = "INVALID_AGE")
     private Integer age;
 
     public MemberRequest() {
