28aea8be638e4d4970889921c7f6b57b7d2f5586
dwl5 <ssop6403@gmail.com>
2021-05-31 15:45:12 +0900

    refactor: 정의하지 않은 예외 처리 수정

--- src/main/java/wooteco/subway/infrastructure/advice/SubwayAdvice.java
+++ src/main/java/wooteco/subway/infrastructure/advice/SubwayAdvice.java
@@ -13,13 +13,12 @@ import wooteco.subway.infrastructure.exception.dto.ErrorResponse;
 public class SubwayAdvice {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleApplicationException(Exception e) throws Exception{
-
         if (AnnotationUtils.findAnnotation(e.getClass(), ResponseStatus.class) != null) {
             throw  e;
         }
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse(e.getMessage()));
+                .body(new ErrorResponse("서버에서 오류가 발생했습니다."));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
