c9e9148cb9b5a41a498e54c54b57aec0c5e03084
Jinhong <xlffm3@gmail.com>
2021-06-01 01:33:57 +0900

    feat: Logger 추가

--- src/main/java/wooteco/subway/exception/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/exception/ExceptionAdvice.java
@@ -1,5 +1,7 @@
 package wooteco.subway.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
+    private static final Logger LOGGER = LoggerFactory.getLogger("file");
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleBindingException(MethodArgumentNotValidException methodArgumentNotValidException) {
@@ -26,12 +29,14 @@ public class ExceptionAdvice {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<String> handleBusinessException(BusinessException businessException) {
+        LOGGER.info(businessException.getMessage());
         return ResponseEntity.status(businessException.getStatus())
                 .body(businessException.getMessage());
     }
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<Void> handleSqlException(SQLException sqlException) {
+        LOGGER.error(sqlException.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .build();
     }
