f468e258e7da5b3590b058dbaee91aaba45c25f8
y_woo :) <dusdn1702@naver.com>
2021-05-31 21:09:48 +0900

    feat: swagger를 통한 api 자동화 코드 구현

--- src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
+++ src/main/java/wooteco/subway/auth/AuthenticationPrincipalConfig.java
@@ -36,7 +36,8 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
-            .addPathPatterns("/**")
+            .addPathPatterns("/api/**")
+            .excludePathPatterns("/v2/api-docs", "/swagger-resources/**", "/swagger-ui.html", "/webjars/**")
             .excludePathPatterns("/api/login/token")
             .excludePathPatterns("/api/members/**")
             .excludePathPatterns("/api/paths/**")
