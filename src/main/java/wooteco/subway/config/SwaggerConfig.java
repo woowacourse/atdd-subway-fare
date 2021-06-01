4a545b45e4ddad358f74abd7fda5a6bd7889ce54
Daeun Lee <leede0418@likelion.org>
2021-06-01 20:22:36 +0900

    fix(config): Swagger base URL 버그 해결

--- src/main/java/wooteco/subway/config/SwaggerConfig.java
+++ src/main/java/wooteco/subway/config/SwaggerConfig.java
@@ -14,6 +14,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .host("da-nyee-subway-fare.kro.kr")
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
