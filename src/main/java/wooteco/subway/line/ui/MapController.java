4b1d59dcf3390e75df78d7695fac9a4ef0abedc2
ROOT <junroot0909@gmail.com>
2021-06-01 21:42:31 +0900

    fix: map api cors 에러 해결

--- src/main/java/wooteco/subway/line/ui/MapController.java
+++ src/main/java/wooteco/subway/line/ui/MapController.java
@@ -2,6 +2,7 @@ package wooteco.subway.line.ui;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import wooteco.subway.line.ui.dto.map.MapResponse;
 
 @RestController
 @RequestMapping("/map")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MapController {
 
     private final LineService lineService;
