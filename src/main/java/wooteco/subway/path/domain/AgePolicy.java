74dec57583ed8b8811420861e6929f6dee343077
joanne <mathmjseo@naver.com>
2021-06-01 20:15:21 +0900

    refactor: AgePolicy 분기 삭제 및 IntPredicate 을 이용하도록 수정

---
+++ src/main/java/wooteco/subway/path/domain/AgePolicy.java
@@ -0,0 +1,32 @@
+package wooteco.subway.path.domain;
+
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+
+public enum AgePolicy {
+    TWENTIES(age -> age > 19, 0, 0),
+    TEENAGERS(age -> 13 <= age  && age < 19, 350, 0.2),
+    PRESCHOOLER(age -> 6 <= age && age < 13, 350, 0.5),
+    BABIES(age -> age < 6, 0, 1);
+
+    private final IntPredicate agePredicate;
+    private final int deductionFare;
+    private final double discountRate;
+
+    AgePolicy(IntPredicate agePredicate, int deductionFare, double discountRate) {
+        this.agePredicate = agePredicate;
+        this.deductionFare = deductionFare;
+        this.discountRate = discountRate;
+    }
+
+    public static AgePolicy of(int age) {
+        return Arrays.stream(AgePolicy.values())
+                .filter(agePolicy -> agePolicy.agePredicate.test(age))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 나이가 존재하지 않습니다."));
+    }
+
+    public int calculateFareAppliedAgePolicy(int currentFare) {
+        return (int) ((currentFare - deductionFare) * (1 - discountRate));
+    }
+}
