c824bb7a52a370cf5ac60129265bc8be24a1c322
RinSabbit <celena1004@naver.com>
2021-06-01 17:40:30 +0900

    fix: CORS 문제 해결

--- src/main/java/wooteco/subway/line/ui/MapController.java
+++ src/main/java/wooteco/subway/line/ui/MapController.java
@@ -2,13 +2,16 @@ package wooteco.subway.line.ui;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wooteco.subway.line.application.LineService;
 import wooteco.subway.line.dto.LineMapResponse;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MapController {
+
     private final LineService lineService;
 
     public MapController(LineService lineService) {
@@ -16,7 +19,7 @@ public class MapController {
     }
 
     @GetMapping("/map")
-    public ResponseEntity<List<LineMapResponse>> findMap(){
+    public ResponseEntity<List<LineMapResponse>> findMap() {
         return ResponseEntity.ok(lineService.findMap());
     }
 }
