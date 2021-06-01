19184569316176ae5c6cc4c74f323593c9b1233f
TaewanKimmmm <ktww88@gmail.com>
2021-06-02 01:32:03 +0900

    refactor: DTO 예외 어노테이션 추가

--- src/main/java/wooteco/subway/member/dto/EmailRequest.java
+++ src/main/java/wooteco/subway/member/dto/EmailRequest.java
@@ -1,6 +1,11 @@
 package wooteco.subway.member.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
 public class EmailRequest {
+    @NotEmpty
+    @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String email;
 
     public EmailRequest() {
--- src/main/java/wooteco/subway/member/dto/MemberRequest.java
+++ src/main/java/wooteco/subway/member/dto/MemberRequest.java
@@ -2,9 +2,20 @@ package wooteco.subway.member.dto;
 
 import wooteco.subway.member.domain.Member;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
+
 public class MemberRequest {
+    @NotEmpty
+    @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String email;
+
+    @NotEmpty(message = "유효하지 않은 패스워드입니다.")
     private String password;
+
+    @NotEmpty
+    @Positive(message = "나이는 양수여야 합니다.")
     private Integer age;
 
     public MemberRequest() {
