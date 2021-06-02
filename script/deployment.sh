73fa3fcb3d57e273dcfae218c2b9a9b180d5289c
Daeun Lee <leede0418@likelion.org>
2021-06-02 15:54:18 +0900

    chore(script): 배포 스크립트 작성

---
+++ script/deployment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /home/ubuntu/atdd-subway-fare
+
+PID=`pidof java`
+kill -9 ${PID}
+
+ORIGIN="https://github.com/da-nyee/atdd-subway-fare"
+BRANCH="step1"
+git pull ${ORIGIN} ${BRANCH}
+
+./gradlew build
+nohup java -jar -Dspring.profiles.active=prod ./build/libs/atdd-subway-fare-0.0.1-SNAPSHOT.jar &
