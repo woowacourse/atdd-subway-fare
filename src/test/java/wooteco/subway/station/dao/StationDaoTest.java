fe15fb4d075fc4ecd9e2a767a0d733599bfd3912
Gomding <backchida@gmail.com>
2021-06-02 22:09:27 +0900

    refactor(StationDaoTest.java) : StationDao 에 대한 테스트 작성 마무리

--- src/test/java/wooteco/subway/station/dao/StationDaoTest.java
+++ src/test/java/wooteco/subway/station/dao/StationDaoTest.java
@@ -10,15 +10,14 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 import wooteco.subway.exception.notfound.StationNotFoundException;
 import wooteco.subway.station.domain.Station;
+import wooteco.subway.station.dto.StationTransferLinesDto;
 
 import javax.sql.DataSource;
-
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("StationDao 관련")
 @DataJdbcTest
@@ -91,7 +90,7 @@ class StationDaoTest {
         assertThat(findStation).isEqualTo(Optional.empty());
     }
 
-    @DisplayName("지하철 역 ID로 지하철 역을 삭제한다.")
+    @DisplayName("존재하지 않는 지하철 역 ID로 지하철 역을 삭제하면 예외가 발생한다..")
     @Test
     void deleteByNotExistId() {
         //when then
@@ -99,16 +98,92 @@ class StationDaoTest {
                 .isInstanceOf(StationNotFoundException.class);
     }
 
+    @DisplayName("지하철 역 ID로 지하철 역 조회한다.")
     @Test
     void findById() {
-        //
+        //given
+        Station insertStation = stationDao.insert(station1);
+        Long savedStationId = insertStation.getId();
+
+        //when
+        Optional<Station> findOptionalStation = stationDao.findById(savedStationId);
+
+        //then
+        assertThat(findOptionalStation.isPresent()).isTrue();
+        Station findStation = findOptionalStation.get();
+        assertThat(findStation.getId()).isEqualTo(insertStation.getId());
+        assertThat(findStation.getName()).isEqualTo(station1.getName());
     }
 
+    @DisplayName("존재하지 않는 지하철 역 ID로 지하철 역 조회한다.")
+    @Test
+    void findByNotExistId() {
+        //when
+        Optional<Station> findOptionalStation = stationDao.findById(Long.MAX_VALUE);
+
+        //then
+        assertThat(findOptionalStation).isEqualTo(Optional.empty());
+    }
+
+    @DisplayName("지하철 역 정보 수정한다.")
     @Test
     void update() {
+        //given
+        Station insertStation = stationDao.insert(station1);
+        Long savedStationId = insertStation.getId();
+
+        //when
+        stationDao.update(new Station(savedStationId, station2.getName()));
+        Station updatedStation = stationDao.findById(savedStationId).get();
+
+        //then
+        assertThat(updatedStation.getId()).isEqualTo(savedStationId);
+        assertThat(updatedStation.getName()).isEqualTo(station2.getName());
     }
 
+    @DisplayName("이미 존재하는 지하철 역 이름으로 지하철 역 정보 수정한다.")
+    @Test
+    void updateByDuplicateName() {
+        //given
+        stationDao.insert(station1);
+        Station insertStation = stationDao.insert(station2);
+        Long savedStationId = insertStation.getId();
+
+        //when then
+        assertThatThrownBy(() -> stationDao.update(new Station(savedStationId, station1.getName())))
+                .isInstanceOf(DuplicateKeyException.class);
+    }
+
+    @DisplayName("존재하지 않는 지하철 역 정보를 수정하면 예외가 발생한다.")
+    @Test
+    void updateByNotExistStation() {
+        //when then
+        assertThatThrownBy(() -> stationDao.update(new Station(Long.MAX_VALUE, station1.getName())))
+                .isInstanceOf(StationNotFoundException.class);
+    }
+
+    @DisplayName("해당 역에서 환승가능한 모든 노선(Line) 정보들을 가진 역 목록을 조회한다.")
     @Test
     void findAllWithTransferLines() {
+        //given
+        Station insertStation1 = stationDao.insert(station1);
+        Station insertStation2 = stationDao.insert(station2);
+
+        //when
+        List<StationTransferLinesDto> allWithTransferLines = stationDao.findAllWithTransferLines();
+
+        //then
+        assertThat(allWithTransferLines).hasSize(2);
+
+        StationTransferLinesDto stationTransferLinesDto1 = allWithTransferLines.get(0);
+        assertThat(stationTransferLinesDto1.getId()).isEqualTo(insertStation1.getId());
+        assertThat(stationTransferLinesDto1.getName()).isEqualTo(insertStation1.getName());
+        assertThat(stationTransferLinesDto1.getTransferLines()).hasSize(0);
+
+        StationTransferLinesDto stationTransferLinesDto2 = allWithTransferLines.get(1);
+
+        assertThat(stationTransferLinesDto2.getId()).isEqualTo(insertStation2.getId());
+        assertThat(stationTransferLinesDto2.getName()).isEqualTo(insertStation2.getName());
+        assertThat(stationTransferLinesDto2.getTransferLines()).hasSize(0);
     }
 }
