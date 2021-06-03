aff104ce482b4e1fd9beba0a53e8eaf665cdf31b
멍토 <daum7766@naver.com>
2021-06-03 23:38:33 +0900

    refactor: 로그를 사용하도록 수정
    RestControllerAdvice 로 변경

--- src/main/java/wooteco/subway/SubwayAdvice.java
+++ src/main/java/wooteco/subway/SubwayAdvice.java
@@ -1,13 +1,17 @@
 package wooteco.subway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import wooteco.subway.exception.SubwayException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class SubwayAdvice {
+
+    private static final Logger log = LoggerFactory.getLogger("console");
 
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<String> handleSubwayCustomException(SubwayException exception) {
@@ -16,8 +20,7 @@ public class SubwayAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception exception) {
-        System.out.println(exception.getClass());
-        System.out.println(exception.getMessage());
+        log.error(exception.getClass() + ": " + exception.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_GATEWAY).body("알 수 없는 에러가 발생했습니다. 관리자에게 문의하세요");
     }
 }
