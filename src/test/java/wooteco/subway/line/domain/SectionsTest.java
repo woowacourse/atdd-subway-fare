214fa0c74e07ccdabba0566514ff7fb5ade89928
Gomding <backchida@gmail.com>
2021-06-03 17:07:14 +0900

    test(SubwayGraphTest.java) : 지하철 경로 조회 그래프에 대한 테스트 작성

---
+++ src/test/java/wooteco/subway/line/domain/SectionsTest.java
@@ -0,0 +1,24 @@
+package wooteco.subway.line.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SectionsTest {
+
+    @Test
+    void addSection() {
+    }
+
+    @Test
+    void getStations() {
+    }
+
+    @Test
+    void getSortSections() {
+    }
+
+    @Test
+    void removeStation() {
+    }
+}
