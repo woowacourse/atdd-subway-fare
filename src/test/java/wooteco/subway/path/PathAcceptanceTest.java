2c0179050f261da3069078baf86fdc2645902552
Wannte <T.wannte@gmail.com>
2021-06-04 22:17:46 +0900

    test(PathAcceptanceTest): 잘못된 입력 테스트 병합
    ParameterizedTest 를 사용하려 했으나, static 으로 methodSource 를 넘겨주는 부분에서 충돌 발생
    (각각의 테스트마다 연결을 하고, 그 연결의 요청으로부터 아이디를 얻는 방법임. method source 를 활용하기 위해서는 static 하게 변수를 선언해 줬어야함.)

--- src/test/java/wooteco/subway/path/PathAcceptanceTest.java
+++ src/test/java/wooteco/subway/path/PathAcceptanceTest.java
@@ -7,6 +7,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import wooteco.subway.AcceptanceTest;
@@ -17,24 +20,26 @@ import wooteco.subway.station.dto.StationResponse;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static wooteco.subway.line.LineAcceptanceTest.지하철_노선_등록되어_있음;
 import static wooteco.subway.line.SectionAcceptanceTest.지하철_구간_등록되어_있음;
 import static wooteco.subway.station.StationAcceptanceTest.지하철역_등록되어_있음;
 
 @DisplayName("지하철 경로 조회")
 public class PathAcceptanceTest extends AcceptanceTest {
-    private LineResponse 신분당선;
-    private LineResponse 이호선;
-    private LineResponse 삼호선;
-    private LineResponse 사호선;
-    private StationResponse 강남역;
-    private StationResponse 양재역;
-    private StationResponse 교대역;
-    private StationResponse 남부터미널역;
-    private StationResponse 다른노선위의역1;
-    private StationResponse 다른노선위의역2;
+    private static LineResponse 신분당선;
+    private static LineResponse 이호선;
+    private static LineResponse 삼호선;
+    private static LineResponse 사호선;
+    private static StationResponse 강남역;
+    private static StationResponse 양재역;
+    private static StationResponse 교대역;
+    private static StationResponse 남부터미널역;
+    private static StationResponse 다른노선위의역1;
+    private static StationResponse 다른노선위의역2;
 
     /**
      * 교대역    --- *2호선* ---   강남역
@@ -62,8 +67,8 @@ public class PathAcceptanceTest extends AcceptanceTest {
         지하철_구간_등록되어_있음(삼호선, 교대역, 남부터미널역, 3);
     }
 
-    @DisplayName("두 역의 최단 거리 경로를 조회한다.")
     @Test
+    @DisplayName("두 역의 최단 거리 경로를 조회한다.")
     void findPathByDistance() {
         ExtractableResponse<Response> response = 거리_경로_조회_요청(교대역.getId(), 양재역.getId());
 
@@ -72,28 +77,16 @@ public class PathAcceptanceTest extends AcceptanceTest {
         총_거리가_응답됨(response, 5, 1250 + expectedExtraFare);
     }
 
+    @Test
     @DisplayName("도달 할 수 없는 두 역의 최단 거리 경로를 조회한다.")
-    @Test
-    void findNonExistPath1() {
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(다른노선위의역1.getId(), 강남역.getId());
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    void findNonExistPath() {
+        assertAll(
+                () -> 잘못된_경로_오류_응답(강남역.getId(), 다른노선위의역2.getId()),
+                () -> 잘못된_경로_오류_응답(강남역.getId(), Long.MAX_VALUE),
+                () -> 잘못된_경로_오류_응답(강남역.getId(), -1L));
     }
 
-    @DisplayName("도달 할 수 없는 두 역의 최단 거리 경로를 조회한다. :: 존재하지않는역 id")
-    @Test
-    void findNonExistPath2() {
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(Long.MAX_VALUE, 강남역.getId());
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    @DisplayName("도달 할 수 없는 두 역의 최단 거리 경로를 조회한다. :: 음의 역 id")
-    @Test
-    void findNonExistPath3() {
-        ExtractableResponse<Response> response = 거리_경로_조회_요청(-1, 강남역.getId());
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-    }
-
-    public static ExtractableResponse<Response> 거리_경로_조회_요청(long source, long target) {
+    private static ExtractableResponse<Response> 거리_경로_조회_요청(long source, long target) {
         return RestAssured
                 .given()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
@@ -103,7 +96,7 @@ public class PathAcceptanceTest extends AcceptanceTest {
                 .extract();
     }
 
-    public static void 적절한_경로_응답됨(ExtractableResponse<Response> response, ArrayList<StationResponse> expectedPath) {
+    private static void 적절한_경로_응답됨(ExtractableResponse<Response> response, ArrayList<StationResponse> expectedPath) {
         PathResponse pathResponse = response.as(PathResponse.class);
 
         List<Long> stationIds = pathResponse.getStations()
@@ -118,7 +111,12 @@ public class PathAcceptanceTest extends AcceptanceTest {
         assertThat(stationIds).containsExactlyElementsOf(expectedPathIds);
     }
 
-    public static void 총_거리가_응답됨(ExtractableResponse<Response> response, int totalDistance, int totalFare) {
+    private void 잘못된_경로_오류_응답(Long fromStationId, Long toStationId) {
+        ExtractableResponse<Response> response = 거리_경로_조회_요청(fromStationId, toStationId);
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private static void 총_거리가_응답됨(ExtractableResponse<Response> response, int totalDistance, int totalFare) {
         PathResponse pathResponse = response.as(PathResponse.class);
         assertThat(pathResponse.getDistance()).isEqualTo(totalDistance);
         assertThat(pathResponse.getFare()).isEqualTo(totalFare);
