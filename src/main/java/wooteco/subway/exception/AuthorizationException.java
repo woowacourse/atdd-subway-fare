07a45eccb1ccc29fb8de10dbf94bb32db88df085
Solji Choi <bssj9307@gmail.com>
2021-06-05 16:25:21 +0900

    refactor: HttpStatus 변경

--- src/main/java/wooteco/subway/exception/AuthorizationException.java
+++ src/main/java/wooteco/subway/exception/AuthorizationException.java
@@ -7,6 +7,6 @@ public class AuthorizationException extends SubwayException {
     private static final String AUTHORIZATION_MESSAGE = "인증되지 않은 사용자입니다.";
 
     public AuthorizationException() {
-        super(HttpStatus.BAD_REQUEST, AUTHORIZATION_MESSAGE);
+        super(HttpStatus.UNAUTHORIZED, AUTHORIZATION_MESSAGE);
     }
 }
