776e0574c4dedd21cc8663dd11b917a3e56e9fee
seunghyun <ggyool@naver.com>
2021-06-06 17:14:43 +0900

    feat: Fare에 equals, hashCode 정의

--- src/main/java/wooteco/subway/path/domain/fare/Fare.java
+++ src/main/java/wooteco/subway/path/domain/fare/Fare.java
@@ -1,5 +1,6 @@
 package wooteco.subway.path.domain.fare;
 
+import java.util.Objects;
 import wooteco.subway.exception.application.ValidationFailureException;
 
 public class Fare {
@@ -40,5 +41,22 @@ public class Fare {
 
     public int value() {
         return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fare fare = (Fare) o;
+        return value == fare.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/wooteco/subway/path/domain/fare/FareTest.java
+++ src/test/java/wooteco/subway/path/domain/fare/FareTest.java
@@ -4,16 +4,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import wooteco.subway.path.domain.fare.Fare;
 
 class FareTest {
 
-    @ParameterizedTest
+    @DisplayName("동등성 검사")
+    @Test
+    void equality() {
+        // when, then
+        Fare fare = new Fare(15, 15, 100);
+        assertThat(fare).isEqualTo(
+            new Fare(15, 15, 100)
+        );
+    }
+
     @DisplayName("노선별 추가 요금 계산")
     @MethodSource("extraFareByLine")
+    @ParameterizedTest
     void distanceRate(int distance, int expectedFare, int age, int extraFare) {
         //when
         Fare fare = new Fare(distance, age, extraFare);
