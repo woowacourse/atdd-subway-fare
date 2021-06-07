65fb892df5c181566aa5e4a79f514d4e8993856d
seunghyun <ggyool@naver.com>
2021-06-07 19:05:21 +0900

    refactor: StationService 리팩터링 및 테스트 추가

--- src/main/java/wooteco/subway/station/application/StationService.java
+++ src/main/java/wooteco/subway/station/application/StationService.java
@@ -1,10 +1,9 @@
 package wooteco.subway.station.application;
 
 import java.util.List;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import wooteco.subway.exception.application.DuplicatedFieldException;
+import wooteco.subway.exception.application.NonexistentTargetException;
 import wooteco.subway.station.dao.StationDao;
 import wooteco.subway.station.domain.Station;
 import wooteco.subway.station.dto.StationRequest;
@@ -20,17 +19,13 @@ public class StationService {
     }
 
     public StationResponse saveStation(StationRequest stationRequest) {
-        Station station;
-        try {
-            station = stationDao.insert(stationRequest.toStation());
-        } catch (DuplicateKeyException e) {
-            throw new DuplicatedFieldException("중복 필드가 있어 역 생성에 실패했습니다.");
-        }
+        Station station = stationDao.insert(stationRequest.toStation());
         return StationResponse.of(station);
     }
 
-    public Station findStationById(Long id) {
-        return stationDao.findById(id);
+    public Station findExistentStationById(Long id) {
+        return stationDao.findById(id)
+            .orElseThrow(() -> new NonexistentTargetException("역ID: " + id));
     }
 
     @Transactional(readOnly = true)
