732512501bcc783af946a9b00ecc9139c58102c1
TaewanKimmmm <ktww88@gmail.com>
2021-06-07 20:35:06 +0900

    refactor: 예외 어노테이션 추가

--- src/main/java/wooteco/subway/member/dto/MemberRequest.java
+++ src/main/java/wooteco/subway/member/dto/MemberRequest.java
@@ -2,20 +2,19 @@ package wooteco.subway.member.dto;
 
 import wooteco.subway.member.domain.Member;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 
 public class MemberRequest {
-    @NotEmpty
+    @NotBlank(message = "이메일 항목을 입력해주세요.")
     @Email(message = "유효하지 않은 이메일 형식입니다.")
     private String email;
 
-    @NotEmpty(message = "유효하지 않은 패스워드입니다.")
+    @NotBlank(message = "비밀번호 항목을 입력해주세요.")
     private String password;
 
-    @NotEmpty
-    @Positive(message = "나이는 양수여야 합니다.")
+    @NotNull(message = "나이 항목을 입력해주세요.")
+    @Min(value = 1, message = "1부터 150 사이의 나이를 입력해주세요")
+    @Max(value = 150, message = "1부터 150 사이의 나이를 입력해주세요")
     private Integer age;
 
     public MemberRequest() {
