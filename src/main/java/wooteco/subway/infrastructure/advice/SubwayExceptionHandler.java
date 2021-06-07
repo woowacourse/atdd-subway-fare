beae60e1eb1f677c46cd7bb249fa0f4037f9a20e
dwl5 <ssop6403@gmail.com>
2021-06-07 13:52:33 +0900

    refactor: logging 추가

--- src/main/java/wooteco/subway/infrastructure/advice/SubwayExceptionHandler.java
+++ src/main/java/wooteco/subway/infrastructure/advice/SubwayExceptionHandler.java
@@ -1,5 +1,7 @@
 package wooteco.subway.infrastructure.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,20 +15,23 @@ import static wooteco.subway.infrastructure.ErrorCode.COMMON_NOT_EXPECTED_ERROR;
 
 @RestControllerAdvice
 public class SubwayExceptionHandler {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler(SubwayException.class)
     public ResponseEntity<ErrorResponse> handlerSubwayException(SubwayException e) {
+        logger.error(e.getErrorMessage());
         return ResponseEntity.status(e.getStatus()).body(ErrorResponse.of(e.getErrorCode()));
     }
 
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        logger.error(COMMON_INVALID_INPUT_VALUE.getError() + " - " + e.getMessage());
         return ResponseEntity.badRequest().body(ErrorResponse.of(COMMON_INVALID_INPUT_VALUE, e.getFieldErrors()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleApplicationException() {
+    public ResponseEntity<ErrorResponse> handleApplicationException(Exception e) {
+        logger.error(COMMON_NOT_EXPECTED_ERROR.getError());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.of(COMMON_NOT_EXPECTED_ERROR));
     }
