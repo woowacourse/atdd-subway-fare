a5ab7f54299731e253e3129eb1cd771ed1c3a19f
jujubebat <bbwwpark@naver.com>
2021-06-07 12:36:31 +0900

    refactor: 인수 테스트 속도 개선 - @DirtiesContext 사용하지 않고, db 데이터를 매번 지우도록 하여 매번 컨테이너가 생성되지 않도록 리팩터링

--- src/test/java/wooteco/subway/AcceptanceTest.java
+++ src/test/java/wooteco/subway/AcceptanceTest.java
@@ -1,21 +1,40 @@
 package wooteco.subway;
 
 import io.restassured.RestAssured;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles("test")
 public class AcceptanceTest {
+
     @LocalServerPort
     int port;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+
+        List<String> sql = Arrays.asList(
+            "delete from section",
+            "delete from station",
+            "delete from line",
+            "delete from member",
+            "ALTER TABLE section ALTER COLUMN id RESTART WITH 1",
+            "ALTER TABLE station ALTER COLUMN id RESTART WITH 1",
+            "ALTER TABLE line ALTER COLUMN id RESTART WITH 1",
+            "ALTER TABLE member ALTER COLUMN id RESTART WITH 1"
+        );
+
+        sql.forEach(jdbcTemplate::update);
     }
 }
