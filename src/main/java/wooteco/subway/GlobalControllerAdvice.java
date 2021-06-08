e83a1a955ec9afe86d0dd3ba44f6dddc2fc725cd
Kimun Kim <kkm97351@gmail.com>
2021-06-08 23:26:52 +0900

    refactor: 예외 발생 시 Logger를 이용해 로깅

--- src/main/java/wooteco/subway/GlobalControllerAdvice.java
+++ src/main/java/wooteco/subway/GlobalControllerAdvice.java
@@ -1,10 +1,10 @@
 package wooteco.subway;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import wooteco.subway.auth.exception.AuthorizationException;
@@ -21,10 +21,11 @@ import java.sql.SQLException;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
+    Logger logger = LoggerFactory.getLogger(GlobalControllerAdvice.class);
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(new ErrorResponse(e.getMessage()));
@@ -32,7 +33,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ErrorResponse> resolveRequest(BindException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(BindErrorResponse.from("인자값을 검증하는 과정에서 오류가 발생했습니다.", e.getBindingResult()));
@@ -40,7 +41,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(MemberDaoException.class)
     public ResponseEntity<ErrorResponse> handleMemberException(BindException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
@@ -48,7 +49,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = {StationDaoException.class, StationDomainException.class})
     public ResponseEntity<ErrorResponse> handleStationException(Exception e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
@@ -56,7 +57,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(value = {LineDaoException.class, LineDomainException.class})
     public ResponseEntity<ErrorResponse> handleLineException(Exception e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
@@ -64,7 +65,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(PathDomainException.class)
     public ResponseEntity<ErrorResponse> handlePathException(PathDomainException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
@@ -72,7 +73,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<ErrorResponse> handleSQLException(SQLException e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("예상치 못한 DB 예외가 발생했습니다." + System.getProperty("line.separator") + "에러 메세지: " + e.getMessage()));
@@ -80,7 +81,7 @@ public class GlobalControllerAdvice {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception e) {
-        e.printStackTrace();
+        logger.error(e.getMessage(), e);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ErrorResponse("예상치 못한 예외가 발생했습니다." + System.getProperty("line.separator") + "에러 메세지: " + e.getMessage()));
