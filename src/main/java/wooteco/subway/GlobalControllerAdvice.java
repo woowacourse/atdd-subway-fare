c714ca921101cec85bde4cf5f72df5297bd940b2
Kimun Kim <kkm97351@gmail.com>
2021-06-09 00:11:45 +0900

    refacor: GlobalControllerAdvice 코드 중복 제거

--- src/main/java/wooteco/subway/GlobalControllerAdvice.java
+++ src/main/java/wooteco/subway/GlobalControllerAdvice.java
@@ -17,19 +17,9 @@ import wooteco.subway.path.exception.PathDomainException;
 import wooteco.subway.station.exception.StationDaoException;
 import wooteco.subway.station.exception.StationDomainException;
 
-import java.sql.SQLException;
-
 @ControllerAdvice
 public class GlobalControllerAdvice {
     Logger logger = LoggerFactory.getLogger(GlobalControllerAdvice.class);
-
-    @ExceptionHandler(AuthorizationException.class)
-    public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity
-                .status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponse(e.getMessage()));
-    }
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ErrorResponse> resolveRequest(BindException e) {
@@ -37,6 +27,14 @@ public class GlobalControllerAdvice {
         return ResponseEntity
                 .badRequest()
                 .body(BindErrorResponse.from("인자값을 검증하는 과정에서 오류가 발생했습니다.", e.getBindingResult()));
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<ErrorResponse> handleAuthorizationException(AuthorizationException e) {
+        logger.error(e.getMessage(), e);
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse(e.getMessage()));
     }
 
     @ExceptionHandler(MemberDaoException.class)
@@ -47,36 +45,12 @@ public class GlobalControllerAdvice {
                 .body(new ErrorResponse(e.getMessage()));
     }
 
-    @ExceptionHandler(value = {StationDaoException.class, StationDomainException.class})
-    public ResponseEntity<ErrorResponse> handleStationException(Exception e) {
+    @ExceptionHandler(value = {StationDaoException.class, StationDomainException.class, LineDaoException.class, LineDomainException.class, PathDomainException.class})
+    public ResponseEntity<ErrorResponse> handleStationException(RuntimeException e) {
         logger.error(e.getMessage(), e);
         return ResponseEntity
                 .badRequest()
                 .body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(value = {LineDaoException.class, LineDomainException.class})
-    public ResponseEntity<ErrorResponse> handleLineException(Exception e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity
-                .badRequest()
-                .body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(PathDomainException.class)
-    public ResponseEntity<ErrorResponse> handlePathException(PathDomainException e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity
-                .badRequest()
-                .body(new ErrorResponse(e.getMessage()));
-    }
-
-    @ExceptionHandler(SQLException.class)
-    public ResponseEntity<ErrorResponse> handleSQLException(SQLException e) {
-        logger.error(e.getMessage(), e);
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorResponse("예상치 못한 DB 예외가 발생했습니다."));
     }
 
     @ExceptionHandler(Exception.class)
