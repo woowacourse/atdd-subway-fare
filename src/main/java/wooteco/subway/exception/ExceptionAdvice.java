997a9c613411172c519118a578e6745e7881bb39
hyeon9mak <jinha3507@gmail.com>
2021-06-09 14:10:01 +0900

    feat: 개발자가 예상치 못한 예외에 대한 핸들링 메서드 추가

--- src/main/java/wooteco/subway/exception/ExceptionAdvice.java
+++ src/main/java/wooteco/subway/exception/ExceptionAdvice.java
@@ -2,6 +2,7 @@ package wooteco.subway.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -25,6 +26,14 @@ public class ExceptionAdvice {
         LOGGER.error(e.getMessage());
 
         return ResponseEntity.badRequest()
+            .body(new ExceptionResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> exceptionResponse(Exception e) {
+        LOGGER.error(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(new ExceptionResponse(e.getMessage()));
     }
 }
