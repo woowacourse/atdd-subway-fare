673849a4a886f45ef414bb963194a1a31c674efe
hyeon9mak <jinha3507@gmail.com>
2021-06-09 14:39:31 +0900

    refactor: 각각 다른 인자로 수행되는 반복 테스트에서 @ParameterizedTest 어노테이션을 사용하도록 변경

--- src/test/java/wooteco/subway/path/FareServiceTest.java
+++ src/test/java/wooteco/subway/path/FareServiceTest.java
@@ -1,35 +1,49 @@
 package wooteco.subway.path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import wooteco.subway.path.application.FareService;
 import wooteco.subway.path.domain.Fare;
 import wooteco.subway.section.domain.Distance;
 
 public class FareServiceTest {
 
-    @Test
     @DisplayName("요금 계산 정책에 맞게 요금 계산이 진행되는지 확인")
-    void calculateFare() {
+    @ParameterizedTest
+    @MethodSource("inputDistanceAndExpectFare")
+    void calculateFare(int distance, int expectedFare) {
         // given
         FareService fareService = new FareService();
 
-        // when
-
         // then
-        assertThat(fareService.calculateFare(new Distance(2))).isEqualTo(new Fare(1250));
-        assertThat(fareService.calculateFare(new Distance(9))).isEqualTo(new Fare(1250));
-        assertThat(fareService.calculateFare(new Distance(10))).isEqualTo(new Fare(1250));
-        assertThat(fareService.calculateFare(new Distance(11))).isEqualTo(new Fare(1250 + 100));
-        assertThat(fareService.calculateFare(new Distance(15))).isEqualTo(new Fare(1250 + 100));
-        assertThat(fareService.calculateFare(new Distance(20))).isEqualTo(new Fare(1250 + 200));
-        assertThat(fareService.calculateFare(new Distance(25))).isEqualTo(new Fare(1250 + 300));
-        assertThat(fareService.calculateFare(new Distance(50))).isEqualTo(new Fare(1250 + 800));
-        assertThat(fareService.calculateFare(new Distance(51))).isEqualTo(new Fare(1250 + 900));
-        assertThat(fareService.calculateFare(new Distance(58))).isEqualTo(new Fare(1250 + 900));
-        assertThat(fareService.calculateFare(new Distance(59))).isEqualTo(new Fare(1250 + 1000));
-        assertThat(fareService.calculateFare(new Distance(67))).isEqualTo(new Fare(1250 + 1100));
+        assertThat(fareService.calculateFare(new Distance(distance))).isEqualTo(new Fare(expectedFare));
+    }
+
+    private static Stream<Arguments> inputDistanceAndExpectFare() {
+        int defaultFare = 1250;
+
+        return Stream.of(
+            arguments(2, defaultFare),
+            arguments(9, defaultFare),
+            arguments(10, defaultFare),
+            arguments(11, defaultFare + 100),
+            arguments(15, defaultFare + 100),
+            arguments(20, defaultFare + 200),
+            arguments(25, defaultFare + 300),
+            arguments(50, defaultFare + 800),
+            arguments(51, defaultFare + 900),
+            arguments(58, defaultFare + 900),
+            arguments(59, defaultFare + 1_000),
+            arguments(67, defaultFare + 1_100)
+        );
     }
 }
