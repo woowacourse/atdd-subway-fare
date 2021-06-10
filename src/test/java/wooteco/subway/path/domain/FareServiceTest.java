9e8eb48b9f31606e456686a4347f1bffa63cf487
Wannte <T.wannte@gmail.com>
2021-06-11 00:54:28 +0900

    test(FareService): parameterized 로 수정

--- src/test/java/wooteco/subway/path/domain/FareServiceTest.java
+++ src/test/java/wooteco/subway/path/domain/FareServiceTest.java
@@ -2,43 +2,31 @@ package wooteco.subway.path.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import wooteco.subway.path.application.FareService;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FareServiceTest {
     private final FareService fareService = new FareService();
 
-    @DisplayName("거리가 7일 때 기본 요금 반환")
-    @Test
-    public void calculateTest1() {
-        int distance = 7;
-        int expectedFare = 1250;
-        assertFare(distance, 0, 30, expectedFare);
+    private static Stream<Arguments> providedFare() {
+        return Stream.of(
+                Arguments.of(7, 0, 1250),
+                Arguments.of(17, 0, 1450),
+                Arguments.of(77, 0, 2450),
+                Arguments.of(7, 1000, 2250)
+        );
     }
 
-    @DisplayName("거리가 17일 때 구간1 반환")
-    @Test
-    public void calculateTest2() {
-        int distance = 17;
-        int expectedFare = 1450;
-        assertFare(distance, 0, 30, expectedFare);
-    }
-
-    @DisplayName("거리가 77일 때 기본 요금 반환")
-    @Test
-    public void calculateTest3() {
-        int distance = 77;
-        int expectedFare = 2450;
-        assertFare(distance, 0, 30, expectedFare);
-    }
-
-    @DisplayName("추가 요금 구간 적용")
-    @Test
-    public void calculateTest4() {
-        int distance = 7;
-        int expectedFare = 1250 + 1000;
-        assertFare(distance, 1000, 30, expectedFare);
+    @ParameterizedTest(name = "[{index}] : 거리 {0}, 노선 추가 요금 {1}일 경우 요금 {2}")
+    @MethodSource("providedFare")
+    public void calculateTest(int distance, int extraFare, int expectedFare) {
+        assertFare(distance, extraFare, 30, expectedFare);
     }
 
     @DisplayName("거리가 7주어지고, 3세의 나이일 때, 아이 요금 적용 반환")
@@ -57,7 +45,7 @@ class FareServiceTest {
         assertFare(distance, 0, 9, expectedFare);
     }
 
-    @DisplayName("거리가 7이 주어지고, 15세 나이 일 때,  청소년 요금 적용 반환")
+    @DisplayName("거리가 7이 주어지고, 15세 나이 일 때, 청소년 요금 적용 반환")
     @Test
     public void calculateTestTeenage() {
         int distance = 7;
